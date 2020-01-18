7dccff4f6725a029b25e46911f66fc111bb3711f
AlexexDenimus <demin.leha1998@yandex.ru>
2020-01-18 15:21:28 +0300

    lesson2

--- homework/lesson2/index.js
+++ homework/lesson2/index.js
@@ -10,22 +10,56 @@
     При отмене – «Отменено»
 */
 
+function authentication() {
+  const userLogin = prompt("Введите ваш логин:");
+  switch (userLogin) {
+    case "Админ":
+      const password = prompt("Введите ваш пароль:");
+      if (!password) {
+        alert("Отменено");
+        break;
+      }
+      password === "Я главный"
+        ? alert("Здравствуйте!")
+        : alert("Неверный пароль");
+      break;
+    case null:
+    case "":
+      alert("Отменено");
+      break;
+    default:
+      alert("Я вас не знаю");
+  }
+}
 
 /*
     2)
     Какие значения выведет цикл while?
 
     Для каждого цикла запишите, какие значения он выведет. Потом сравните с ответом.
-    Оба цикла выводят alert с одинаковыми значениями или нет?
+    Оба цикла выводят alert с одинаковыми значениями или нет? - нет
 
-    префиксный вариант ++i
+    префиксный вариант ++i  - выведет 1 2 3 4
     let i = 0;
     while (++i < 5) alert( i );
 
-    постфиксный вариант i++
+    постфиксный вариант i++ выведет 1 2 3 4 5
     let i = 0;
     while (i++ < 5) alert( i );
 */
+
+const prefixCycle = (callback = postfixCycle) => {
+  let i = 0;
+  alert("Префиксный вариант");
+  while (++i < 5) alert(i);
+  callback();
+};
+
+const postfixCycle = () => {
+  let i = 0;
+  alert("Постфиксный вариант");
+  while (i++ < 5) alert(i);
+};
 
 /*
     3)
@@ -35,6 +69,13 @@
         alert( `number ${i}!` );
     }
 */
+
+const changeForToWhile = () => {
+  let i = 0;
+  while (i < 3) {
+    alert(`number ${i++}!`);
+  }
+};
 
 /*
     4) Перепишите код с использованием одной конструкции switch:
@@ -54,6 +95,22 @@
     }
 */
 
+const useSwitch = () => {
+  const number = +prompt("Введите число между 0 и 3", "");
+  switch (number) {
+    case 0:
+      alert("Вы ввели число 0");
+      break;
+    case 1:
+      alert("Вы ввели число 1");
+      break;
+    case 2:
+    case 3:
+      alert("Вы ввели число 2, а может и 3");
+      break;
+  }
+};
+
 /*
     5)
     Перепишите функцию, чтобы она делала то же самое, но без if, в одну строку.
@@ -67,8 +124,23 @@
     }
 */
 
+function getBackgroundColor(theme) {
+  return theme === "light" ? "#FFF" : "#000";
+}
+
+const testFifthTask = () => {
+  const theme = prompt("Введите тип темы:");
+  alert(getBackgroundColor(theme));
+};
+
 /**
  * 6) Напишите функцию pow(x,n), которая возвращает x в степени n
  */
 
- 
+const pow = (x, n) => x ** n;
+
+const testSixthTask = () => {
+  const x = prompt("Введите число для возведения в степень:", 2);
+  const n = prompt("В какую степень необходимо возвести число?", 10);
+  alert(pow(x, n));
+};
