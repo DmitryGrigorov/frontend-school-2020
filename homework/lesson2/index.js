180b6bf3d27865db3c48c00dd84703255fbf1824
yuliya <yuliya.sm7@gmail.com>
2020-01-18 09:51:34 +0300

    DZ2

--- homework/lesson2/index.js
+++ homework/lesson2/index.js
@@ -9,7 +9,27 @@
     Иначе – «Неверный пароль»
     При отмене – «Отменено»
 */
+(function() {
+    const login = prompt("Введите логин:");
+    if (login === 'Админ') {
+        const password = prompt("Введите пароль:");
+        check();
+    } else if (login === '') {
+        alert("Отменено");
+    } else {
+        alert("Я вас не знаю");
+    }
+})()
 
+function check(password) {
+    if (password === "Я главный") {
+        alert("Здравствуйте!");
+    } else if (password === '') {
+        allert('Отменено');
+    } else {
+        alert("Неверный пароль");
+    }
+}
 
 /*
     2)
@@ -26,7 +46,13 @@
     let i = 0;
     while (i++ < 5) alert( i );
 */
+(function() {
+    let i = 0;
+    while (++i < 5) alert(i); // 1 2 3 4
 
+    let j = 0;
+    while (j++ < 5) alert(j); // 1 2 3 4 5
+})()
 /*
     3)
     Перепишите код, заменив цикл for на while, без изменения поведения цикла.
@@ -35,6 +61,12 @@
         alert( `number ${i}!` );
     }
 */
+
+let i = 0;
+while (i < 3) {
+    alert(`number ${i}!`);
+    i++;
+}
 
 /*
     4) Перепишите код с использованием одной конструкции switch:
@@ -54,6 +86,21 @@
     }
 */
 
+const number = prompt('Введите число между 0 и 3');
+switch (number) {
+    case '0':
+        alert('Вы ввели число 0');
+        break;
+    case '1':
+        alert('Вы ввели число 1');
+        break;
+    case '2':
+    case '3':
+        alert('Вы ввели число 2, а может и 3');
+    default:
+        alert("Неверное число");
+}
+
 /*
     5)
     Перепишите функцию, чтобы она делала то же самое, но без if, в одну строку.
@@ -67,8 +114,16 @@
     }
 */
 
+function getBackgroundColor(theme) {
+    return theme == 'light' ? '#FFF' : '#000';
+}
+alert(getBackgroundColor("light"));
+
 /**
  * 6) Напишите функцию pow(x,n), которая возвращает x в степени n
  */
 
- 
+function pow(x, n) {
+    return x ** n;
+}
+alert(pow(3, 3));
