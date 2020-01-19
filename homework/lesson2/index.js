1703b50ea6b256dc81f5593e0cba0b7a1b52f339
Dmitry Razorenov <razorenov.dmitry@yandex.ru>
2020-01-19 05:13:21 +0300

    lesson2 is ready

--- homework/lesson2/index.js
+++ homework/lesson2/index.js
@@ -9,7 +9,35 @@
     Иначе – «Неверный пароль»
     При отмене – «Отменено»
 */
-
+/*
+let login = prompt('Введите логин');
+let result = '';
+switch(login){
+    case 'Админ':
+        let password = prompt('Введите пароль');
+        switch(password){
+            case 'Я главный': 
+                result = 'Здравствуйте!'; 
+                break;
+            default:
+                if(typeof(password)=="string" && password.length > 0){
+                    result = 'Неверный пароль';
+                    break;
+                }
+                result = 'Отменено';
+        }
+        break;
+    default:
+        if (typeof(login)=="string" && login.length > 0){
+            result = 'Я вас не знаю';
+            break;
+        }
+        result = 'Отменено';
+        break;
+}
+ 
+alert(result);
+*/
 
 /*
     2)
@@ -22,9 +50,13 @@
     let i = 0;
     while (++i < 5) alert( i );
 
+    1,2,3,4
+
     постфиксный вариант i++
     let i = 0;
     while (i++ < 5) alert( i );
+
+    1,2,3,4,5
 */
 
 /*
@@ -35,6 +67,9 @@
         alert( `number ${i}!` );
     }
 */
+
+// let i = 0; 
+// while (++i<3) alert( `number ${i}!` );
 
 /*
     4) Перепишите код с использованием одной конструкции switch:
@@ -53,7 +88,23 @@
         alert('Вы ввели число 2, а может и 3');
     }
 */
-
+/*
+const number = +prompt('Введите число между 0 и 3', '');
+let is0 = (number === 0);
+let is1 = (number === 1);
+let is2_3 = ((number === 2) || (number === 3));
+switch(true){
+    case is0: 
+        alert('Вы ввели число 0');
+        break;
+    case is1:
+        alert('Вы ввели число 1');
+        break;
+    case is2_3:
+        alert('Вы ввели число 2, а может и 3');
+        break;
+}
+*/
 /*
     5)
     Перепишите функцию, чтобы она делала то же самое, но без if, в одну строку.
@@ -66,9 +117,21 @@
         }
     }
 */
-
+/*
+function getBackgroundColor(theme) {
+    return (theme === 'light') ? '#FFF' : '#000';
+}
+*/
 /**
  * 6) Напишите функцию pow(x,n), которая возвращает x в степени n
  */
 
- 
+/*
+function pow(x,n){
+    return x ** n;
+} 
+
+let x = prompt('Введите число');
+let n = prompt('Введите степень');
+alert(pow(x,n));
+*/
