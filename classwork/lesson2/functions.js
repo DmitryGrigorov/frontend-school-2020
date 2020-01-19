46db9a27ceaf41af06c369cbfdec7240a423fdd2
Viktor Khan <victory_96@mail.ru>
2020-01-19 13:40:59 +0300

    Classwork's exercises from file functions.js

--- classwork/lesson2/functions.js
+++ classwork/lesson2/functions.js
@@ -1,11 +1,26 @@
 // напишите функцию checkAge которая принимает аргумент age и возвращает true если age больше 18, а иначе возвращает false
-
+function    checkAge(age) {
+    if (age > 18) {
+        return true;
+    } else {
+        return false;
+    }
+}
 // Напишите функцию max(a,b), которая возвращает большее из чисел a и b.
-
+function max(a,b) { 
+    if (a > b) { // первый вариант
+        return a; 
+    } else {
+        return b;
+    }
+    // return a > b ? a : b; второй вариант 
+}
 // Замените код Function Expression стрелочной функцией
 let wrapToStars = function(str) {
     return '*' + string + '*';
 }
+
+//let wrapToStars = (str) => '*' + string + '*';
 
 let transformToNumber = function(str) {
     if (isNaN(Number(str))) {
@@ -14,3 +29,12 @@ let transformToNumber = function(str) {
 
     return Number(str);
 }
+
+/*
+let transformToNumber = (str) => {
+    if (isNaN(Number(str))) {
+        return 0;
+    } 
+    return Number(str);
+}
+*/
