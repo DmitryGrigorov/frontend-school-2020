b580459055e32d00e397af63b41be5fe616d71e3
Viktor Khan <victory_96@mail.ru>
2020-01-19 13:29:28 +0300

    Classwork's exercises from file compare.js

--- classwork/lesson2/compare.js
+++ classwork/lesson2/compare.js
@@ -1,10 +1,10 @@
-let a = 5 > 4; // ??
-let b = "ананас" > "яблоко"; // ??
-let c = "2" > "12"; // ??
-let d = undefined == null; // ??
-let e = undefined === null; // ??
-let f = null == "\n0\n"; // ??
-let g = null === +"\n0\n"; // ??
+let a = 5 > 4; // true
+let b = "ананас" > "яблоко"; // false
+let c = "2" > "12"; // true сравнение двух строк, сравнение идет посимвольно
+let d = undefined == null; // true при нестрогом равенстве
+let e = undefined === null; // false
+let f = null == "\n0\n"; // false
+let g = null === +"\n0\n"; // false
 
 
 // alert, prompt, confirm
