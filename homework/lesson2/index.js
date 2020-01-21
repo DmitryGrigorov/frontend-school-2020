56780ea182b13f5f05271d726a04f787515a9201
aasim313 <ababenko@bk.ru>
2020-01-21 18:59:00 +0300

    less2 home. test4

--- homework/lesson2/index.js
+++ homework/lesson2/index.js
@@ -89,6 +89,24 @@ while (i < 3) {
         alert('Вы ввели число 2, а может и 3');
     }
 */
+/*
+const number = +prompt("Введите число между 0 и 3", "");
+switch (number) {
+  case 0:
+    alert("Вы ввели число 0");
+    break;
+  case 1:
+    alert("Вы ввели число 1");
+    break;
+  case 2 || 3:
+    alert("Вы ввели число 2, а может и 3");
+    break;
+
+  default:
+    break;
+}
+
+*/
 
 /*
     5)
