8d2a0906a80eda99990518b26ffe65b80ecd072d
a.karelin <karelin_alex@inbox.ru>
2020-01-22 17:52:34 +0300

    Добавил ДЗ 3

--- homework/lesson3/index.js
+++ homework/lesson3/index.js
@@ -5,6 +5,12 @@
     lwTail("ВАСЯ") == "Вася";
 */
 
+function lwTail(str){
+    return str.substr(0, 1) + str.substr(1).toLowerCase();
+}
+
+console.log('первая буква', lwTail('ВАСЯ'));
+
 /*
     2)
     Дана строка; нужно написать функцию, которая позволяет вернуть значение true,
@@ -16,6 +22,13 @@
     palindrome('table') === false
     palindrome('John') === false
 */
+
+function palindrom(str) {
+    const reverse = str.toLowerCase().split('').reverse().join('');
+    return (str.toLowerCase()) === reverse;
+}
+
+['racecar', 'Anna', 'table', 'John'].forEach((item) => console.log(item, palindrom(item)));
 
 /*
     3)
@@ -37,6 +50,9 @@
     console.log( fruits.length ); // ?
 */
 
+/********************************************************************************************************/
+// ОТВЕТ массивы - объекты, а значит передаются по ссылке, т.е. независимо от того, через какую переменную массив муотруется результат должне быть одинаковым т.е. ответ 4;
+/********************************************************************************************************/
 
 /*
     4)
@@ -53,6 +69,13 @@
     потом переделайте всё как нужно и методом join соедините обратно.
 */
 
+function camelize(str) {
+    const parts = str.split('-');
+    const modifiedParts = parts.map((item) => lwTail(item.toUpperCase()));
+    return modifiedParts.join('');
+}
+
+["background-color", "list-style-image", "-webkit-transition"].forEach(item => console.log(item, camelize(item)));
 
 /*
     5)
@@ -68,6 +91,18 @@
     alert( arr ); // HTML, JavaScript, CSS (без изменений)
 */
 
+function copySorted(arr) {
+    let newArr = arr.slice();
+    return newArr.sort();
+}
+
+let arr = ["HTML", "JavaScript", "CSS"];
+
+let sorted = copySorted(arr);
+
+console.log( sorted ); // CSS, HTML, JavaScript
+console.log( arr ); // HTML, JavaScript, CSS (без изменений)
+
 /*
     6)
     Написать функцию, которая считает сумму элементов массива кратных числу 2.
@@ -77,3 +112,16 @@
     sum(1, 2, 3, 4, 5) // 6
     sum(3, 8, 1, 40, 6) // 54
 */
+
+function sum() {
+    let args = [];
+    for (var i = 0; i < arguments.length; i++) {
+        args[i] = arguments[i];
+    }
+    return args.reduce((acc, value) => {
+        return value%2===0 ? acc + value : acc;
+    }, 0);
+}
+
+console.log("сумма", sum(1, 2, 3, 4, 5));
+console.log("сумма", sum(3, 8, 1, 40, 6));
