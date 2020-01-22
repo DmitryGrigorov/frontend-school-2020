6d984cb7c86fd069d99d948dd9ecf9c3445f4874
Novikov Stanislav <novikov.stan@mail.ru>
2020-01-22 15:33:04 +0300

    homework2 done

--- homework/lesson2/index.js
+++ homework/lesson2/index.js
@@ -9,7 +9,15 @@
     Иначе – «Неверный пароль»
     При отмене – «Отменено»
 */
-
+function loginChecker() {
+  let login = prompt("Введите Ваш логин:", "");
+  if (login === "" || login === null) alert("Отменено");
+  else if (login === "Админ") {
+    let password = prompt("Введите пароль:");
+    if (password === "Я главный") alert("Здравствуйте");
+    else alert("Неверный пароль");
+  } else alert("Я Вас не знаю");
+}
 
 /*
     2)
@@ -21,11 +29,20 @@
     префиксный вариант ++i
     let i = 0;
     while (++i < 5) alert( i );
+    Ответ: 1 2 3 4
 
     постфиксный вариант i++
     let i = 0;
     while (i++ < 5) alert( i );
+    Ответ: 1 2 3 4 5
 */
+
+function whileChecker() {
+  let i = 0;
+  while (++i < 5) alert(i);
+  i = 0;
+  while (i++ < 5) alert(i);
+}
 
 /*
     3)
@@ -35,6 +52,14 @@
         alert( `number ${i}!` );
     }
 */
+
+function forToWhile() {
+  let i = 0;
+  while (i < 3) {
+    alert(`number ${i}!`);
+    i++;
+  }
+}
 
 /*
     4) Перепишите код с использованием одной конструкции switch:
@@ -54,6 +79,24 @@
     }
 */
 
+function ifToSwitch() {
+  const number = +prompt("Введите число между 0 и 3", "");
+  switch (number) {
+    case 0:
+      alert("Вы ввели число 0");
+      break;
+    case 1:
+      alert("Вы ввели число 1");
+      break;
+    case 2:
+    case 3:
+      alert("Вы ввели число 2, а может и 3");
+      break;
+    default:
+      alert("Введено неверное число.");
+  }
+}
+
 /*
     5)
     Перепишите функцию, чтобы она делала то же самое, но без if, в одну строку.
@@ -67,8 +110,20 @@
     }
 */
 
+function getBackgroundColor(theme) {
+  theme === "light" ? "#FFF" : "#000";
+}
+
 /**
  * 6) Напишите функцию pow(x,n), которая возвращает x в степени n
  */
 
- 
+function pow(x, n) {
+  if (n > 0) {
+    return n != 1 ? x * pow(x, n - 1) : x;
+  } else if (n < 0) {
+    return n != -1 ? 1/x * pow(x, n + 1) : 1/x;
+  } else return 1;
+}
+
+console.log(pow(2, 0));
