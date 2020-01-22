f8163b44a5d22b92ebccc0eae7aae3d0cd25e1a7
Dmitry Razorenov <razorenov.dmitry@yandex.ru>
2020-01-22 19:55:32 +0300

    lesson 3 homework is ready

--- homework/lesson3/index.js
+++ homework/lesson3/index.js
@@ -4,6 +4,10 @@
 
     lwTail("ВАСЯ") == "Вася";
 */
+function lwTail(str){
+    return str[0].toUpperCase()+str.slice(1).toLowerCase();
+}
+// alert(lwTail(prompt('Введите строку')));
 
 /*
     2)
@@ -16,6 +20,18 @@
     palindrome('table') === false
     palindrome('John') === false
 */
+
+function palindrome(str = ''){
+    if(str.length < 3)
+        return false;
+    str = str.toUpperCase();
+    
+    return (str == str.split('').reverse().join(''));
+}
+// console.log('"racecar" is palindrome: ' + palindrome('racecar'));// === true
+// console.log('"Anna" is palindrome: ' + palindrome('Anna'));// === true
+// console.log('"table" is palindrome: ' + palindrome('table'));// === false
+// console.log('"John" is palindrome: ' + palindrome('John'));// === false
 
 /*
     3)
@@ -35,6 +51,8 @@
 
     // что в fruits?
     console.log( fruits.length ); // ?
+
+    Код вернет: 4. shoppingCart не копия, а ссылка на массив fruits
 */
 
 
@@ -53,6 +71,17 @@
     потом переделайте всё как нужно и методом join соедините обратно.
 */
 
+function camelize(str){
+    let arr = str.split('-');
+
+    for (let i=1; i<arr.length; i++)
+        arr[i] = arr[i][0].toUpperCase() + arr[i].slice(1);
+
+    return arr.join(''); 
+}
+// console.log(camelize("background-color"));
+// console.log(camelize("list-style-image"));
+// console.log(camelize("-webkit-transition"));
 
 /*
     5)
@@ -68,6 +97,16 @@
     alert( arr ); // HTML, JavaScript, CSS (без изменений)
 */
 
+function copySorted(arr){
+    return arr.slice().sort();
+}
+let arr = ["HTML", "JavaScript", "CSS"];
+
+let sorted = copySorted(arr);
+
+// alert( sorted ); // CSS, HTML, JavaScript
+// alert( arr ); // HTML, JavaScript, CSS (без изменений)
+
 /*
     6)
     Написать функцию, которая считает сумму элементов массива кратных числу 2.
@@ -77,3 +116,15 @@
     sum(1, 2, 3, 4, 5) // 6
     sum(3, 8, 1, 40, 6) // 54
 */
+
+function sum(){
+    return Object.values(arguments).reduce(function(summary, current){
+            if(typeof(current) == 'number' && current % 2 == 0)
+                    return summary = summary + current;
+            return summary;    
+        }, 0);
+
+}
+
+// console.log(sum(1, 2, 3, 4, 5)); // 6
+// console.log(sum(3, 8, 1, 40, 6)); // 54
