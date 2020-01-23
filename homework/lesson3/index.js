d33fa648d4a2c72d89d79a62aa1f6a83cbf2757d
Irina <irinakir98@gmail.com>
2020-01-23 14:54:51 +0300

    Сделано дз #3

--- homework/lesson3/index.js
+++ homework/lesson3/index.js
@@ -5,6 +5,13 @@
     lwTail("ВАСЯ") == "Вася";
 */
 
+function lwTail (str) {
+    str = str[0].toUpperCase() + str.slice(1).toLowerCase();
+    return str;
+}
+
+console.log(lwTail("ВАСЯ"));
+
 /*
     2)
     Дана строка; нужно написать функцию, которая позволяет вернуть значение true,
@@ -16,6 +23,12 @@
     palindrome('table') === false
     palindrome('John') === false
 */
+
+function palindrome(word) {
+    return word.toLowerCase() === word.toLowerCase().split('').reverse().join('')
+}
+
+console.log( palindrome('Шалаш') );
 
 /*
     3)
@@ -37,6 +50,11 @@
     console.log( fruits.length ); // ?
 */
 
+// Ответ: 
+// в fruits: Яблоки, Груша, Апельсин, Банан
+// console.log( fruits.length ); равно 4
+// shoppingCart и fruits являются ссылками на один и тот же массив
+
 
 /*
     4)
@@ -53,6 +71,18 @@
     потом переделайте всё как нужно и методом join соедините обратно.
 */
 
+function camelize(str) {
+    str = str.split('-');
+
+    str = str.map((item, index) => (index === 0) ?  item : item[0].toUpperCase() + item.slice(1));
+
+    str = str.join('');
+    return str;
+}
+
+console.log( camelize("list-style-image") );
+console.log( camelize("background-color") );
+console.log( camelize("-webkit-transition") );
 
 /*
     5)
@@ -68,6 +98,31 @@
     alert( arr ); // HTML, JavaScript, CSS (без изменений)
 */
 
+//Развернутый вариант решения
+/* 
+function copySorted(arr) {
+    let copyArray = []; 
+
+    for (let i = 0; i < arr.length; i++) {
+        copyArray[i]  = arr[i];
+    };
+
+    return copyArray.sort();
+
+}
+*/
+
+function copySorted(arr) {
+    return arr.slice().sort();
+}
+
+let arr = ["HTML", "JavaScript", "CSS"];
+let sorted = copySorted(arr);
+
+console.log( arr );
+console.log( sorted );
+console.log( arr === sorted ); // Получим false, т.к. sorted копия
+
 /*
     6)
     Написать функцию, которая считает сумму элементов массива кратных числу 2.
@@ -77,3 +132,19 @@
     sum(1, 2, 3, 4, 5) // 6
     sum(3, 8, 1, 40, 6) // 54
 */
+
+function sum(arr) {
+    let sumItem = 0;
+    arr.forEach(item => {
+        if (item % 2 === 0) {
+            sumItem += item;
+        }
+    });
+    return sumItem;
+}
+
+
+console.log( sum([1, 2, 3, 4, 5]) );
+console.log( sum([3, 8, 1, 40, 6]) );
+
+
