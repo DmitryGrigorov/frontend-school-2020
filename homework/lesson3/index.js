6014bf2519a8cbab5afa5b1c8a8ff46639b71900
dmitrii <drozdov.dn29@gmail.com>
2020-01-26 19:20:59 +0300

    homework lesson 3

--- homework/lesson3/index.js
+++ homework/lesson3/index.js
@@ -4,7 +4,23 @@
 
     lwTail("ВАСЯ") == "Вася";
 */
+/*  ---------
+    1) Ответ:
 
+    const lwTail = str => {
+        let str2,i=0;
+        str2 =str[0];
+        while (i<str.length) {
+            if (i>0) {
+                str2 += str[i].toLowerCase();
+            }
+            i++;
+        }
+        return str2;
+    }
+    
+    console.log(lwTail("ВАСЯ HGFJGHDKGH kddkdk FJDKFJ"));
+    */
 /*
     2)
     Дана строка; нужно написать функцию, которая позволяет вернуть значение true,
@@ -16,10 +32,17 @@
     palindrome('table') === false
     palindrome('John') === false
 */
+/*  ---------
+    2) Ответ:
 
+    const palindrome = str => {
+        return (str.toLowerCase() === str.toLowerCase().split('').reverse().join(''));
+    };
+    console.log(palindrome('1nna'));
+*/
 /*
-    3)
-    Что выведет следующий код?
+    // 3)
+    // Что выведет следующий код?
 
     let fruits = ["Яблоки", "Груша", "Апельсин"];
 
@@ -34,9 +57,14 @@
     fruits.pop()
 
     // что в fruits?
-    console.log( fruits.length ); // ?
-*/
+    console.log( fruits.length ); //  ["Яблоки", "Груша", "Апельсин", "Банан"]
 
+ */
+/*  ---------
+    3) Ответ:
+
+        ["Яблоки", "Груша", "Апельсин", "Банан"]
+*/
 
 /*
     4)
@@ -52,8 +80,27 @@
     P.S. Подсказка: используйте split, чтобы разбить строку на массив символов,
     потом переделайте всё как нужно и методом join соедините обратно.
 */
+/*
+    ---------
+    4) Ответ:
 
+    const camelize = str => {
+        let str2 = '';
+        let i = 0;
+        str2 = str.split('');
+        while(i < str2.length) {
+            if (str2[i] === '-') {
+                delete str2[i];
+                str2[i+1] = str2[i+1].toUpperCase();
+            }
+            i++;
+        }
+        return str2.join('');
+    }
 
+        console.log(camelize('-webkit-transition'));
+
+*/
 /*
     5)
     У нас есть массив строк arr. Нужно получить отсортированную копию, но оставить arr неизменённым.
@@ -68,12 +115,51 @@
     alert( arr ); // HTML, JavaScript, CSS (без изменений)
 */
 
-/*
-    6)
-    Написать функцию, которая считает сумму элементов массива кратных числу 2.
+/* ---------
+    5) Ответ:
 
-    Например:
+    let arr = ["HTML", "JavaScript", "CSS"];
 
-    sum(1, 2, 3, 4, 5) // 6
-    sum(3, 8, 1, 40, 6) // 54
+    let copySorted = arr => {
+            let arr2 = [];
+            let i =0;
+            while (i < arr.length) {
+                arr2[i] = arr[i];
+                i++;
+            }
+            return arr2.sort();
+        }
+        let sorted = copySorted(arr);
+        console.log(sorted);
+        console.log(arr);
 */
+/*
+        6)
+        Написать функцию, которая считает сумму элементов массива кратных числу 2.
+
+        Например:
+        
+        sum(1, 2, 3, 4, 5) // 6
+        sum(3, 8, 1, 40, 6) // 54
+    */
+
+/* ---------
+   6) Ответ:
+    
+    let sum = [3, 8, 1, 40, 6]; 
+
+    const sumFun = arr => {
+        let i = 0;
+        let sum = 0;
+        while (i < arr.length) {
+            if (arr[i] % 2 === 0) {
+                sum += arr[i];
+            }
+            i++;
+        }
+        return sum;
+    };
+    let s = sumFun(sum);
+    console.log(s);
+
+    */
