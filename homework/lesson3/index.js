0ff35fc548db27b57c5e0139a0b867ac223682cb
amaze-surprise <iksmile@yandex.ru>
2020-01-27 00:25:05 +0300

    'commit'

--- homework/lesson3/index.js
+++ homework/lesson3/index.js
@@ -2,8 +2,21 @@
     1)
     Напишите функцию lwTail(str), возвращающую строку str в нижнем регистры все буквы кроме первой. Например:
 
-    lwTail("ВАСЯ") == "Вася";
-*/
+    */
+
+    
+
+/*let namestr = prompt("Введите любое слово в любом регистре");
+
+function lwTail(namestr) {
+    let endstring = namestr.toLowerCase();
+    return endstring[0].toUpperCase() + endstring.slice(1);
+}
+
+let result = lwTail(namestr);
+
+document.write(result);*/
+
 
 /*
     2)
@@ -15,7 +28,23 @@
     palindrome('Anna') === true
     palindrome('table') === false
     palindrome('John') === false
+
 */
+
+/*let string = prompt("Введите слово для проверки");
+
+function Search (string) {
+
+    let strLen = string.split('')
+    strLen = strLen.reverse();
+    strLen = strLen.join('');
+    let res = (strLen===string) ? true : false;
+    return res;
+}
+
+let result = Search(string);
+
+document.write(result);*/
 
 /*
     3)
@@ -36,7 +65,20 @@
     // что в fruits?
     console.log( fruits.length ); // ?
 */
+let fruits = ["Яблоки", "Груша", "Апельсин"];
 
+    // добавляем новое значение в "копию"
+    let shoppingCart = fruits;
+
+    shoppingCart.push("Банан");
+
+    shoppingCart.push("Манго");
+
+    // удаляем значения в старом массиве
+    fruits.pop()
+
+    // что в fruits?
+    console.log( fruits.length );
 
 /*
     4)
@@ -52,8 +94,29 @@
     P.S. Подсказка: используйте split, чтобы разбить строку на массив символов,
     потом переделайте всё как нужно и методом join соедините обратно.
 */
+/*
+let repl = prompt("Введите слово для преобразования");
+
+function replWord (repl) {
+    let wordrepl = repl.split("-");
+    let firstarray = wordrepl[0];
+    wordrepl.shift(); //Выводится массив без первого слова
+    newarray = [];
+
+    newarray.forEach(function (item) {
+        newArr.push(item.charAt(0).toUpperCase()+item.slice(1));
+        }
+    );
 
 
+    return firstarray+newarray.join();
+}
+
+let result = replWord(repl);
+console.log(result);
+document.write(result);
+
+*/
 /*
     5)
     У нас есть массив строк arr. Нужно получить отсортированную копию, но оставить arr неизменённым.
