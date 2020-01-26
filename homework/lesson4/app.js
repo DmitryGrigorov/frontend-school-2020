72dfbab2efa74feb5ed6464ad8d5aa145d6ca853
Victor Stupnikov <goldfish2012@ya.ru>
2020-01-26 19:39:56 +0300

    Revert "Revert "HomeWork. Lesson 3""
    This reverts commit 362941c62b481a3d488ce97d52771e8ca5dff624.

--- homework/lesson4/app.js
+++ homework/lesson4/app.js
@@ -11,6 +11,23 @@
 // calculate('lemon', 2, { apple: 100, pear: 500, melon: 400, lemon: undefined }); // Извините, товар закончился!
 // calculate('pear', 4, { apple: 100, pear: 500, melon: 400, lemon: undefined }); // 2000
 
+function calculate(name, quantity, prices) {
+    if (name in prices) {
+        if (prices[name] === undefined)
+            return 'Извините, товар закончился!';
+        else
+            return prices[name] * quantity;
+    }
+    else
+        return  'Такого товара у нас ещё нет!';
+}
+
+console.log('# 1)');
+console.log(calculate('potato', 1, {apple: 100, pear: 500, melon: 400, lemon: undefined})); // Такого товара у нас еще нет!
+console.log(calculate('lemon', 2, {apple: 100, pear: 500, melon: 400, lemon: undefined})); // Извините, товар закончился!
+console.log(calculate('pear', 4, {apple: 100, pear: 500, melon: 400, lemon: undefined})); // 2000
+
+
 // 2)
 // напишите функцию deepClone глубокого клонирования объекта, которая создаёт глубокую копию объекта
 // * - глубокая копия - это значит, что если внутри объекта есть свойства объекты - их нужно тоже склонировать
@@ -21,6 +38,30 @@
 // cloneSomeObj === someObj // false при сравнении копия и первоначальный объект не равны
 // cloneSomeObj.metrics === someObj.metrics // false при сравнении вложенного объекта они тоже не равны
 
+function deepClone(obj) {
+    if(typeof(obj) !== 'object') {
+        console.log(obj + ' Не является объектом!');
+        return;
+    }
+    let clone = {};
+    for (let property in obj){
+        if(typeof (obj[property]) === 'object' && obj[property] !== null){
+            clone[property] = deepClone(obj[property]);
+        } else {
+            clone[property] = obj[property];
+        }
+    }
+    return clone;
+}
+console.log('\n# 2)');
+let someObj = {name: 'Petya', metrics: {weight: 80, height: 180}}; // есть такой объект
+let cloneSomeObj = deepClone(someObj); // создаем его копию
+console.log('someObj:'); console.log(someObj);
+console.log('cloneSomeObj:'); console.log(cloneSomeObj);
+console.log('cloneSomeObj === someObj: ' + (cloneSomeObj === someObj).toString()); // false при сравнении копия и первоначальный объект не равны)
+console.log('cloneSomeObj.metrics === someObj.metrics: ' + (cloneSomeObj.metrics === someObj.metrics).toString()); // false при сравнении вложенного объекта они тоже не равны
+
+
 // 3)
 // напишите функцию merge для объединения объектов НЕ используя встроеный метод Object.assign
 // колличество передаваемых аргументов в функцию НЕ ограничено (вложенные объекты копируются по ссылке)
@@ -28,6 +69,21 @@
 // Например:
 // let unionObject = merge({}, { name: 'Vasya' }, { age: 45 }, { isAdmin: true });
 // unionObject -> { name: 'Vasya', age: 45, isAdmin: true }
+
+console.log('\n# 3)');
+function merge(...args) {
+    let object = {};
+    for (let obj of args) {
+        for (let prop in obj) {
+            object[prop] = obj[prop]; //Прим.: совпадающие свойства в объектах перезаписываются (из крайнего объекта)
+        }
+    }
+    return object;
+}
+let unionObject = merge({}, { name: 'Vasya' }, { age: 45 }, { isAdmin: true }); //Объединение четырёх объектов
+console.log('merge(unionObject): ');
+console.log(merge(unionObject)); // { name: 'Vasya', age: 45, isAdmin: true }
+
 
 // 4)
 // Есть объект dog = { name: 'Bobik' };
@@ -40,12 +96,38 @@
 // dog.bark(4); // => "Bobik: bark bark bark bark"
 // dog.bark(); // => "Bobik: bark" если аргумент не передать - метод все равно сработает
 
+console.log('\n# 4)');
+let dog = {name: 'Bobik'};
+dog.bark = function (numberOfTimes) {
+    if(numberOfTimes === undefined) numberOfTimes = 1;
+    let bark = Array(numberOfTimes + 1 ).join('bark ').trim(); // Создаёт строку длиной в нужное количество "bark"
+    console.log(`${this.name}: ${bark}`);
+}
+console.log('dog.bark(4):')
+dog.bark(4); // => "Bobik: bark bark bark bark"
+console.log('dog.bark():')
+dog.bark(); // => "Bobik: bark" если аргумент не передать - метод все равно сработает
+
+
 // 5)
 // Есть объект товара item = { label: 'phone', price: 500, currency: '$' };
 // сделайте так, чтобы при преобразовании данного объекта
 // к строке возвращалась строка => "500$",
 // а при преобразовании к числе возвращалось просто 500
 // обратите внимание, что 500 и $ это значения полей самого объекта (если их поменять то это будет учитываться при последующих преобразованиях)
+
+console.log('\n# 5)');
+let item = { label: 'phone', price: 500, currency: '$' };
+item[Symbol.toPrimitive] = function (type) {
+    if (type === 'string') {
+        return `${this.price}${this.currency}`;
+    } else if (type === 'number') {
+        return this.price;
+    }
+}
+console.log('String(item): ' + String(item));
+console.log('Number(item): ' + Number(item));
+
 
 // 6)
 // напишите конструктор Dog который создает объект со свойствами name, age, breed, weight, height, position, status
@@ -59,3 +141,59 @@
 // dog.down() => Меняет свойство status на строку 'lying';
 //
 // создайте массив с 25 объектами Dog
+
+function Dog(name, age, breed, weight, height, position, status) {
+   this.name = name;
+   this.age = age;
+   this.breed = breed;
+   this.weight = weight;
+   this.height = height;
+   this.position = position;
+   this.status = status;
+
+   this.bark = function () {
+       console.log(`{${this.name}}: bark`)
+   }
+   this.place = function () {
+       this.position = 'place'
+   }
+   this.come = function () {
+       this.position = 'here'
+   }
+   this.goOut = function () {
+       this.position = 'go out'
+   }
+   this.sit = function () {
+       this.status = 'sitting'
+   }
+   this.stand = function () {
+       this.status = 'standing'
+   }
+   this.down = function () {
+       this.status = 'lying'
+   }
+}
+
+console.log('\n# 6)')
+let doggy = new Dog('Скуби-Ду', 1, 'Немецкий дог', 2, 3, 'place', 'standing');
+console.log(doggy);
+doggy.bark();
+doggy.place();
+doggy.come();
+doggy.goOut();
+doggy.sit();
+doggy.stand();
+doggy.down();
+
+let dogs = [];
+for (let i = 0; i < 25; i++){
+    dogs.push(new Dog(
+        'DogName' + (i + 1),
+        i === 0 ? 1 : dogs[i-1].age + 0.5,
+        'Пудель',
+        5 + i,
+        10 + i,
+        'place',
+        'sitting'))
+}
+console.log(dogs);
