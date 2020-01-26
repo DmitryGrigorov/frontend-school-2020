b45b60d9b98673634f1b8d2289ab878bfafa36e9
Victor Stupnikov <goldfish2012@ya.ru>
2020-01-26 19:39:59 +0300

    Revert "Revert "HomeWork. Lesson 3""
    This reverts commit abe93206b5e9bd1a8f662dd007ef93f13c7f6c7e.

--- homework/lesson3/index.js
+++ homework/lesson3/index.js
@@ -5,6 +5,12 @@
     lwTail("ВАСЯ") == "Вася";
 */
 
+function lwTail(str) {
+    return str[0] + str.substr(1).toLowerCase();
+}
+console.log( '# 1)\n'+ lwTail('лЮБОВЬ') );
+
+
 /*
     2)
     Дана строка; нужно написать функцию, которая позволяет вернуть значение true,
@@ -16,6 +22,13 @@
     palindrome('table') === false
     palindrome('John') === false
 */
+
+function palindrome(str){
+    return str.toLowerCase() === str.toLowerCase().split('').reverse().join('');
+}
+console.log('\n# 2)\n' + 'palindrome("Коту хорош и шорох уток"): ' + palindrome('Коту хорош и шорох уток'));
+console.log('palindrome("Коту хорош и шорох уток."): ' + palindrome('Коту хорош и шорох уток.'));
+
 
 /*
     3)
@@ -37,6 +50,9 @@
     console.log( fruits.length ); // ?
 */
 
+console.log('\n# 3) ' + 'Будет выведено число 4 - количество элементов в массиве fruits (["Яблоки", "Груша", "Апельсин", "Банан"])');
+console.log('Переменные shoppingCart и fruits ссылаются на один и тот же объект (массив).');
+
 
 /*
     4)
@@ -53,6 +69,21 @@
     потом переделайте всё как нужно и методом join соедините обратно.
 */
 
+function camelize(str) {
+    let arr = str.split('-');
+    let index;
+    for (index = 0; index < arr.length; ++index){
+        arr[index] = index > 0 ?
+            arr[index][0].toUpperCase() + arr[index].substr(1, arr[index].length - 1 ) :
+            arr[index]
+    }
+    return arr.join('');
+}
+
+console.log('\n# 4) ' + 'camelize("background-color"): ' + camelize('background-color'));
+console.log('camelize("list-style-image"): ' + camelize('list-style-image'));
+console.log('camelize("-webkit-transition"): ' + camelize('-webkit-transition'));
+
 
 /*
     5)
@@ -68,6 +99,16 @@
     alert( arr ); // HTML, JavaScript, CSS (без изменений)
 */
 
+function copySorted(arr){
+    return arr.slice().sort();
+}
+let arr = ["HTML", "JavaScript", "CSS"];
+let sorted = copySorted(arr);
+
+console.log('\n# 5) ' + 'copySorted(arr): ' + copySorted(arr) );
+console.log('                 arr: ' + arr);
+
+
 /*
     6)
     Написать функцию, которая считает сумму элементов массива кратных числу 2.
@@ -77,3 +118,14 @@
     sum(1, 2, 3, 4, 5) // 6
     sum(3, 8, 1, 40, 6) // 54
 */
+
+function sum(arr) {
+    return arr.reduce( (sum, current) =>
+            current % 2 === 0 ?
+                sum + current :
+                sum,
+        0);
+}
+
+console.log('\n# 6) ' + 'sum(1, 2, 3, 4, 5): ' + sum([1, 2, 3, 4, 5]) );
+console.log('sum(3, 8, 1, 40, 6): ' + sum([3, 8, 1, 40, 6]) );
