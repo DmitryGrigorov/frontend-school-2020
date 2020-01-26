bfb0f1503fa18f8b07717f06b4eae23180ebf6a9
Denis Stepanov <nahamshabun@gmail.com>
2020-01-26 20:39:00 +0300

    Add lesson3 homework

--- homework/lesson3/index.js
+++ homework/lesson3/index.js
@@ -5,6 +5,10 @@
     lwTail("ВАСЯ") == "Вася";
 */
 
+function lwTail(str) {
+    return str.charAt(0).toUpperCase() + str.substring(1).toLowerCase();
+}
+
 /*
     2)
     Дана строка; нужно написать функцию, которая позволяет вернуть значение true,
@@ -16,6 +20,10 @@
     palindrome('table') === false
     palindrome('John') === false
 */
+
+function isPalindrome(str) {
+    return str.toLowerCase() === str.toLowerCase().split('').reverse().join('');
+}
 
 /*
     3)
@@ -37,6 +45,7 @@
     console.log( fruits.length ); // ?
 */
 
+4
 
 /*
     4)
@@ -53,6 +62,19 @@
     потом переделайте всё как нужно и методом join соедините обратно.
 */
 
+function camelize(str) {
+	let strAsArray = str.split('');
+	let result = [];
+
+	for (let i = 0; i < strAsArray.length; i++) {
+		if (strAsArray[i] === '-') {
+			strAsArray[i + 1] = strAsArray[i + 1].toUpperCase();
+		} else {
+			result.push(strAsArray[i]);
+		}
+	}
+	return result.join('');
+}
 
 /*
     5)
@@ -68,6 +90,10 @@
     alert( arr ); // HTML, JavaScript, CSS (без изменений)
 */
 
+function copySorted(arr) {
+	return arr.slice().sort();
+}
+
 /*
     6)
     Написать функцию, которая считает сумму элементов массива кратных числу 2.
@@ -77,3 +103,15 @@
     sum(1, 2, 3, 4, 5) // 6
     sum(3, 8, 1, 40, 6) // 54
 */
+
+function sum(...nums) {
+	let result = 0;
+	
+	for (let num of nums) {
+		if (num % 2 === 0) {
+			result += num;
+		}
+	}
+	
+	return result;
+}
