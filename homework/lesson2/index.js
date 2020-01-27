15e60ccc0e6c77637b4aaf2efc5395e894b8ed88
Andrey Shavyrin <shavyrin1605@gmail.com>
2020-01-27 20:14:46 +0300

    add hw

--- homework/lesson2/index.js
+++ homework/lesson2/index.js
@@ -9,6 +9,25 @@
     Иначе – «Неверный пароль»
     При отмене – «Отменено»
 */
+let password;
+let login;
+login = prompt("Введите логин", '');
+if (login === 'Админ') {
+    password = prompt("Введите пароль", '');
+} else if (login) {
+    alert('Я вас не знаю')
+} else {
+    alert('Отменено')
+}
+
+if (password === "Я главный") {
+    alert('Здравствуйте!')
+} else if (password) {
+    alert('Неверный пароль')
+} else {
+    alert('Отменено')
+}
+
 
 
 /*
@@ -27,6 +46,10 @@
     while (i++ < 5) alert( i );
 */
 
+
+alert('В первом случае i=5, во втором i=6')
+
+
 /*
     3)
     Перепишите код, заменив цикл for на while, без изменения поведения цикла.
@@ -35,7 +58,10 @@
         alert( `number ${i}!` );
     }
 */
-
+let i = -1;
+while (++i < 3) {
+    alert(`number ${i}!`)
+}
 /*
     4) Перепишите код с использованием одной конструкции switch:
 
@@ -53,7 +79,19 @@
         alert('Вы ввели число 2, а может и 3');
     }
 */
-
+const number = +prompt('Введите число между 0 и 3', '');
+switch (number) {
+    case 0:
+        alert('Вы ввели число 0');
+        break;
+    case 1:
+        alert('Вы ввели число 1');
+        break;
+    case 2:
+    case 3:
+        alert('Вы ввели число 2, а может и 3');
+        break;
+}
 /*
     5)
     Перепишите функцию, чтобы она делала то же самое, но без if, в одну строку.
@@ -66,9 +104,17 @@
         }
     }
 */
-
+function getBackgroundColor(theme) {
+    return (theme === 'light') ? '#FFF' : '#000';
+}
 /**
  * 6) Напишите функцию pow(x,n), которая возвращает x в степени n
  */
-
- 
+function pow(x, n) {
+    let power = 1;
+    let i = -1
+    while (++i < n) {
+        power *= x;
+    }
+    return power
+}
--- homework/lesson3/index.js
+++ homework/lesson3/index.js
@@ -4,18 +4,33 @@
 
     lwTail("ВАСЯ") == "Вася";
 */
-
+function lwTail(str) {
+    return str[0].toUpperCase() + str.slice(1).toLowerCase();
+}
 /*
-    2)
-    Дана строка; нужно написать функцию, которая позволяет вернуть значение true,
-    если строка является палиндромом, и false — если нет. 
-    При этом нужно учитывать пробелы и знаки препинания.
+2)
+Дана строка; нужно написать функцию, которая позволяет вернуть значение true,
+если строка является палиндромом, и false — если нет.
+При этом нужно учитывать пробелы и знаки препинания.
 
-    palindrome('racecar') === true
-    palindrome('Anna') === true
-    palindrome('table') === false
-    palindrome('John') === false
+palindrome('racecar') === true
+palindrome('Anna') === true
+palindrome('table') === false
+palindrome('John') === false
+
 */
+
+function palindrome(text) {
+    text = text.toLowerCase();
+    let center = Math.floor(text.length / 2);
+    let count = text.length;
+    for (let i = 0; i < center; i++) {
+        if (text[i] != text[count - 1 - i]) {
+            return false;
+        }
+    }
+    return true
+}
 
 /*
     3)
@@ -37,6 +52,7 @@
     console.log( fruits.length ); // ?
 */
 
+alert(4);
 
 /*
     4)
@@ -52,7 +68,17 @@
     P.S. Подсказка: используйте split, чтобы разбить строку на массив символов,
     потом переделайте всё как нужно и методом join соедините обратно.
 */
-
+function camelize(str) {
+    let arr = str.split('');
+    let count = arr.length;
+    for (let i = 0; i < count; i++) {
+        if (arr[i] === '-') {
+            let letter = arr[i + 1].toUpperCase();
+            arr.splice(i, 2, letter);
+        }
+    }
+    return arr.join('');
+}
 
 /*
     5)
@@ -68,6 +94,22 @@
     alert( arr ); // HTML, JavaScript, CSS (без изменений)
 */
 
+function copySorted(arr) {
+    let temp;
+    let count = arr.length;
+    let copyArr = arr.slice(0);
+    for (let i = 0; i < count - 1; i++) {
+        for (let j = i + 1; j < count; j++) {
+            if (copyArr[j] < copyArr[i]) {
+                temp = copyArr[i];
+                copyArr[i] = copyArr[j];
+                copyArr[j] = temp;
+            }
+        }
+    }
+    return copyArr;
+}
+
 /*
     6)
     Написать функцию, которая считает сумму элементов массива кратных числу 2.
@@ -77,3 +119,13 @@
     sum(1, 2, 3, 4, 5) // 6
     sum(3, 8, 1, 40, 6) // 54
 */
+function sum(arr) {
+    let sum = 0;
+    for (obj in arr) {
+        if (arr[obj] % 2 === 0) {
+            sum += arr[obj];
+            console.log(sum);
+        }
+    }
+    return sum;
+}
