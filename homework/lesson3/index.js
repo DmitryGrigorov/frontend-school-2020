50e71d19635f04319f9eac41b36336a0a59b5f61
Андрей <gmgm.ab@gmail.com>
2020-01-27 15:13:31 +0300

    added homework lesson3

--- homework/lesson3/index.js
+++ homework/lesson3/index.js
@@ -5,6 +5,13 @@
     lwTail("ВАСЯ") == "Вася";
 */
 
+function lwTail(str) {
+    let firstLetter = str.substring(0, 1).toUpperCase();
+    let tailLetters = str.substring(1, str.length).toLowerCase();
+
+    return firstLetter + tailLetters;
+};
+
 /*
     2)
     Дана строка; нужно написать функцию, которая позволяет вернуть значение true,
@@ -16,6 +23,9 @@
     palindrome('table') === false
     palindrome('John') === false
 */
+
+const isPalindrome = string => string.toLowerCase().split('').reverse().join('') === string.toLowerCase();
+console.log(isPalindrome("Racecar"));
 
 /*
     3)
@@ -37,6 +47,8 @@
     console.log( fruits.length ); // ?
 */
 
+/*Переменная shoppingCart хранит ссылку на массив fruits.
+Оба массива содержат ["Яблоки", "Груша", "Апельсин", "Банан"]*/
 
 /*
     4)
@@ -53,6 +65,24 @@
     потом переделайте всё как нужно и методом join соедините обратно.
 */
 
+function lwTail(str) {
+    let firstLetter = str.substring(0, 1).toUpperCase();
+    let tailLetters = str.substring(1, str.length).toLowerCase();
+
+    return firstLetter + tailLetters;
+};
+
+function camelize(str) {
+    let array = str.split("-");
+    array.forEach((elem, index) => {
+        array[index] = (index < 1) ? elem.toLowerCase() : lwTail(elem);
+    })
+
+    return array.join("")
+}
+
+const word = "list-style-image"
+console.log(camelize(word));
 
 /*
     5)
@@ -68,6 +98,14 @@
     alert( arr ); // HTML, JavaScript, CSS (без изменений)
 */
 
+const copySorted = arr => arr.slice().sort();
+
+let arr = ["HTML", "JavaScript", "CSS"];
+let sorted = copySorted(arr);
+
+alert( sorted ); // CSS, HTML, JavaScript
+alert( arr ); // HTML, JavaScript, CSS (без изменений)
+
 /*
     6)
     Написать функцию, которая считает сумму элементов массива кратных числу 2.
@@ -77,3 +115,9 @@
     sum(1, 2, 3, 4, 5) // 6
     sum(3, 8, 1, 40, 6) // 54
 */
+
+countSum = arr => arr.reduce((accumulator, currentValue) => currentValue % 2 === 0 ?
+    accumulator + currentValue : accumulator, 0);
+
+const arr = [3, 8, 1, 40, 6]
+console.log(countSum(arr))
