75986c6e96cafd12cc1947556370b2bb6de52a17
Alexandra Venetsky <air@guestwifir15.gst.get-com.ru>
2020-01-27 16:34:18 +0300

    add homework to lesson3

--- homework/lesson3/index.js
+++ homework/lesson3/index.js
@@ -4,7 +4,12 @@
 
     lwTail("ВАСЯ") == "Вася";
 */
-
+    function lwTail(str)
+    {
+        if (!str) return str;
+        return str[0].toUpperCase() + str.slice(1).toLowerCase();
+    }
+    console.log(lwTail("TEST"));
 /*
     2)
     Дана строка; нужно написать функцию, которая позволяет вернуть значение true,
@@ -16,7 +21,19 @@
     palindrome('table') === false
     palindrome('John') === false
 */
-
+    function palindrome(str)
+    {
+        let strReverse = str.split('').reverse().join('').toLowerCase();
+        if(strReverse === str.toLowerCase())
+        {
+            return true;
+        }
+        else 
+        {
+            return false;
+        }
+    }
+    console.log(palindrome('racecar racecar'));
 /*
     3)
     Что выведет следующий код?
@@ -36,8 +53,7 @@
     // что в fruits?
     console.log( fruits.length ); // ?
 */
-
-
+    console.log("Длина масива 4, так это не копия, а ссылка на массив");
 /*
     4)
     Напишите функцию camelize(str), которая преобразует строки вида «my-short-string» в «myShortString».
@@ -53,7 +69,11 @@
     потом переделайте всё как нужно и методом join соедините обратно.
 */
 
-
+    function camelize(str)
+    {
+        return str.split('-').map((word, ind)=> ind==0?word:(word[0].toUpperCase()+word.slice(1).toLowerCase())).join('');
+    }
+    console.log(camelize("-webkit-transition"));
 /*
     5)
     У нас есть массив строк arr. Нужно получить отсортированную копию, но оставить arr неизменённым.
@@ -67,7 +87,16 @@
     alert( sorted ); // CSS, HTML, JavaScript
     alert( arr ); // HTML, JavaScript, CSS (без изменений)
 */
+    function copySorted(arr)
+    {
+        return arr.slice().sort();
+    }
+     let arr = ["HTML", "JavaScript", "CSS"];
 
+    let sorted = copySorted(arr);
+
+    console.log( sorted ); // CSS, HTML, JavaScript
+    console.log( arr ); // HTML, JavaScript, CSS (без изменений)
 /*
     6)
     Написать функцию, которая считает сумму элементов массива кратных числу 2.
@@ -77,3 +106,15 @@
     sum(1, 2, 3, 4, 5) // 6
     sum(3, 8, 1, 40, 6) // 54
 */
+    function sum(...args)
+    {
+        let result=0;
+        let arr = args;
+        for (let val of arr)
+        {
+            if(val%2==0)
+                result+=val;
+        }
+        return result;
+    }
+console.log(sum(1, 2, 3, 4, 5));
