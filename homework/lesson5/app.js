e8885f977d80aad4c67e739a1b3e54acc06d7ca6
Dmitry Razorenov <razorenov.dmitry@yandex.ru>
2020-01-29 00:36:15 +0300

    homework lesson 5 is ready - w/o call/apply/bind

--- homework/lesson5/app.js
+++ homework/lesson5/app.js
@@ -8,7 +8,21 @@
   sumTo(3) = 3 + 2 + 1 = 6
   sumTo(4) = 4 + 3 + 2 + 1 = 10
 */
+function sumTo(n){  
+  let sum;
 
+  if(n > 1){
+    sum = n + sumTo(n - 1);
+  } else {
+    sum = n;
+  }
+  return sum;
+}
+
+// console.log(sumTo(1));
+// console.log(sumTo(2));
+// console.log(sumTo(3));
+// console.log(sumTo(4));
 /*
   2)
   Последовательность чисел Фибоначчи определяется формулой Fn = Fn-1 + Fn-2. То есть, следующее число получается как сумма двух предыдущих.
@@ -26,6 +40,37 @@
   alert(fib(7)); // 13
   alert(fib(77)); // 5527939700884757
 */
+
+function fib(n){
+  let value;
+  
+  if(n < 1){
+    return null;
+  }else if (n === 2 || n === 1){
+    return 1;
+  }
+
+  let arrFib = [];
+  arrFib.push(1, 1);
+
+  function calcFib(count){
+    arrFib[arrFib.length]  = arrFib[arrFib.length-1] + arrFib[arrFib.length-2]
+    
+    if(arrFib.length < n ){
+      calcFib(count-1);
+    }
+
+    return arrFib[arrFib.length-1];
+  } 
+  value = calcFib(n);
+
+  return value;
+}
+
+// alert(fib(1)); // 1
+// alert(fib(3)); // 2
+// alert(fib(7)); // 13
+// alert(fib(77)); // 5527939700884757
 
 /* 
   3) Следующая функция вычисляет сумму передаваемых ей аргументов,
@@ -49,7 +94,19 @@
 
   Используя rest-операторы, перепишите эту функцию так, чтобы не использовать arguments.
 */
+function sum(...rest) {
+  let sum = 0;
 
+  sum = rest.reduce((s, elem) => s + elem, 0);
+  
+  return sum;
+}
+
+// Examples:
+
+// console.log(sum(1, 2, 3));  // 6
+// console.log(sum(5, 5));  // 10
+// console.log(sum(10, 20, 3, 7)); // 40
 /*
   4) Напишите функцию add, которая работает, как на примере:
 
@@ -57,6 +114,18 @@
   add(2)(5) // 7
 */
 
+function add(a, b = null){  
+  function addCore(b){
+    return a + b;
+  }
+ 
+  if(b === null)
+    return addCore;
+
+  return addCore(b);
+}
+// console.log(add(2, 5));
+// console.log(add(2)(5));
 /*
   5)
   Написать функцию sortOdd(), которая сортирует только четные числа в массиве:
@@ -64,7 +133,25 @@
   // Examples:
   sort([9, 8, 7, 6, 5, 4, 3, 2, 1]) // [9, 2, 7, 4, 5, 6, 3, 8, 1]
 */
+function sortOdd(arr = []){
+  let indexOdd = [];
+  let arrOdd = arr.filter((el, i, arr) => {
+    if (el % 2 == 0){
+      indexOdd.push(i);
+      return el;
+    }
+  });
 
+  arrOdd.sort((a, b) => a - b);
+  
+  indexOdd.forEach((key, i, indexOdd) => {
+    arr[key] = arrOdd[i];
+  })
+
+  return arr;
+}
+
+// console.log(sortOdd([9, 8, 7, 6, 5, 4, 3, 2, 1])); // [9, 2, 7, 4, 5, 6, 3, 8, 1]
 /*
   6)
   Написать функцию findMax(), которая найдет максимальный элемент из аргументов:
@@ -73,6 +160,15 @@
   findMax(9, 8, 7, 6, 5, 4, 3, 2, 1) // 9
   findMax(0, 2, 12, 4, 5) // 12
 */
+
+function findMax(...rest){
+  rest.sort((a, b) => a - b);
+  
+  return rest[rest.length - 1];
+}
+
+// console.log(findMax(9, 8, 7, 6, 5, 4, 3, 2, 1)) // 9
+// console.log(findMax(0, 2, 12, 4, 5)) // 12
 
 /*
   7)
@@ -83,6 +179,14 @@
   findMax(0, 2, 12, 4, 5) // 0
 */
 
+function findMin(...rest){
+  rest.sort((a, b) => a - b);
+  
+  return rest[0];
+}
+
+// console.log(findMin(9, 8, 7, 6, 5, 4, 3, 2, 1)) // 1
+// console.log(findMin(0, 2, 12, 4, 5)) // 0
 
 /*
   8)
@@ -92,3 +196,21 @@
   pushZeros([0, 9, 0, 8, 7, 0, 6, 5, 4, 3, 2, 1]) // [9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 0, 0]
   pushZeros([0, 2, 12, 4, 5]) // [2, 12, 4, 5, 0]
 */
+function pushZeros(arr = []){
+  let zeroCount = 0;
+  arr = arr.filter((el, i ,arr) => {
+    if(el !== 0){
+      return el;
+    }else{
+      zeroCount++;
+    }
+  });
+
+  for(i = 0; i< zeroCount; i++)
+    arr.push(0);
+
+  return arr;
+}
+
+// console.log(pushZeros([0, 9, 0, 8, 7, 0, 6, 5, 4, 3, 2, 1])); // [9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 0, 0]
+// console.log(pushZeros([0, 2, 12, 4, 5])); // [2, 12, 4, 5, 0]
