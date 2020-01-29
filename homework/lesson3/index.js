1f162dc989f0253ef47b3d435ef7350806a40491
Andrey Shavyrin <shavyrin1605@gmail.com>
2020-01-29 12:38:02 +0300

    complete lesson 3

--- homework/lesson3/index.js
+++ homework/lesson3/index.js
@@ -109,6 +109,12 @@ function copySorted(arr) {
     }
     return copyArr;
 }
+let arr = ["HTML", "JavaScript", "CSS"];
+
+let sorted = copySorted(arr);
+
+alert( sorted ); // CSS, HTML, JavaScript
+alert( arr ); // HTML, JavaScript, CSS (без изменений)
 
 /*
     6)
