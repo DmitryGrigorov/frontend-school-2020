f54c8fa518865a29d24620a6943c58d50cd1cfce
@anninwonderland <asrakina@miem.hse.ru>
2020-01-29 20:34:43 +0300

    rollback:)

--- classwork/lesson6/app.js
+++ classwork/lesson6/app.js
@@ -1,18 +1,5 @@
 // конструктор с прототипом
 
-function Car () {
-    this.state = 'stopped';
-
-}
-Car.prototype.run = function () {
-    this.state = 'running';
-}
-
-let myCar = new Car()
-console.log(myCar.run());
-
-
-
 // способы задания прототипов
 
 // встроенные объекты, полифилы
