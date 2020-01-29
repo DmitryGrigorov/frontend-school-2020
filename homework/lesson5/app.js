ecbaa7940a7fb6e973f1c3d0fe3c79d1c5676a19
AlexexDenimus <demin.leha1998@yandex.ru>
2020-01-29 15:31:28 +0300

    finish leson5

--- homework/lesson5/app.js
+++ homework/lesson5/app.js
@@ -9,6 +9,8 @@
   sumTo(4) = 4 + 3 + 2 + 1 = 10
 */
 
+const sumTo = n => (n !== 1 ? n + sumTo(n - 1) : n);
+
 /*
   2)
   Последовательность чисел Фибоначчи определяется формулой Fn = Fn-1 + Fn-2. То есть, следующее число получается как сумма двух предыдущих.
@@ -26,6 +28,11 @@
   alert(fib(7)); // 13
   alert(fib(77)); // 5527939700884757
 */
+
+function fib(n) {
+  if (n <= 2) return 1;
+  return fib(n - 1) + fib(n - 2);
+}
 
 /* 
   3) Следующая функция вычисляет сумму передаваемых ей аргументов,
@@ -50,6 +57,12 @@
   Используя rest-операторы, перепишите эту функцию так, чтобы не использовать arguments.
 */
 
+function sum(...rest) {
+  return rest.reduce((previous, current) => {
+    return previous + current;
+  }, 0);
+}
+
 /*
   4) Напишите функцию add, которая работает, как на примере:
 
@@ -57,13 +70,33 @@
   add(2)(5) // 7
 */
 
+function add(a, b) {
+  if (!b)
+    return function(c) {
+      return a + c;
+    };
+  return a + b;
+}
+
 /*
   5)
   Написать функцию sortOdd(), которая сортирует только четные числа в массиве:
 
   // Examples:
-  sort([9, 8, 7, 6, 5, 4, 3, 2, 1]) // [9, 2, 7, 4, 5, 6, 3, 8, 1]
+  sortOdd([9, 8, 7, 6, 5, 4, 3, 2, 1]) // [9, 2, 7, 4, 5, 6, 3, 8, 1]
 */
+
+const sortOdd = array => {
+  const oddArray = array
+    .filter((value, index) => {
+      if (index % 2 !== 0) return value;
+    })
+    .sort();
+
+  let i = 0;
+
+  return array.map((value, index) => (index % 2 !== 0 ? oddArray[i++] : value));
+};
 
 /*
   6)
@@ -74,15 +107,18 @@
   findMax(0, 2, 12, 4, 5) // 12
 */
 
+const findMax = (...rest) => Math.max.call(null, ...rest);
+
 /*
   7)
   Написать функцию findMin(), которая найдет минимальный элемент из аргументов:
 
   // Examples:
-  findMax(9, 8, 7, 6, 5, 4, 3, 2, 1) // 1
-  findMax(0, 2, 12, 4, 5) // 0
+  findMin(9, 8, 7, 6, 5, 4, 3, 2, 1) // 1
+  findMin(0, 2, 12, 4, 5) // 0
 */
 
+const findMin = (...rest) => Math.min.call(null, ...rest);
 
 /*
   8)
@@ -92,3 +128,13 @@
   pushZeros([0, 9, 0, 8, 7, 0, 6, 5, 4, 3, 2, 1]) // [9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 0, 0]
   pushZeros([0, 2, 12, 4, 5]) // [2, 12, 4, 5, 0]
 */
+
+const pushZeros = array => {
+  const zeroArray = [];
+  return array
+    .filter(value => {
+      if (value !== 0) return value;
+      zeroArray.push(value);
+    })
+    .concat(zeroArray);
+};
