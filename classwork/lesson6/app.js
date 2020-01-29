1ffca41646d5b37cecf0a898f37d06ce62efb625
@anninwonderland <asrakina@miem.hse.ru>
2020-01-29 20:32:50 +0300

    Lesson 5: add ex 1-3

--- classwork/lesson6/app.js
+++ classwork/lesson6/app.js
@@ -1,5 +1,18 @@
 // конструктор с прототипом
 
+function Car () {
+    this.state = 'stopped';
+
+}
+Car.prototype.run = function () {
+    this.state = 'running';
+}
+
+let myCar = new Car()
+console.log(myCar.run());
+
+
+
 // способы задания прототипов
 
 // встроенные объекты, полифилы
