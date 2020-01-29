08420473b33e0f3bf60ca718e63a3a66a895cf52
MAX <madmax007@yandex.ru>
2020-01-29 19:59:58 +0300

    --- Auto-Git Commit ---

--- classwork/lesson6/app.js
+++ classwork/lesson6/app.js
@@ -1,4 +1,16 @@
 // конструктор с прототипом
+function Car{
+
+
+
+
+}
+Car.prototype.run = function{
+    console.log('running');
+}
+console.log(Car === Car.prototype.const);
+const someCar = new Car();
+console.log(someCar);
 
 // способы задания прототипов
 
