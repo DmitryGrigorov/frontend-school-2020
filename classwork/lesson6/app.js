1a6a18409697f53a1ce2282ab0a7a22d6dc47978
Kobold <zhr771@gmail.com>
2020-02-01 12:02:29 +0300

    Finish homework for lesson 6

--- classwork/lesson6/app.js
+++ classwork/lesson6/app.js
@@ -1,20 +1,25 @@
 // конструктор с прототипом
-// function Car() {
-//   this.state = 'stopped';
-// }
+function Car() {
+  this.state = "stopped";
+}
 
-// Car.prototype.run = function() {
-//   this.state = 'running';
-// }
+Car.prototype.run = function() {
+  this.state = "running";
+};
+
+// console.log(Car.prototype);
 
 // console.log(Car === Car.prototype.constructor);
-// const someCar = new Car();
+const someCar = new Car();
 // console.log(someCar);
 // someCar.run();
 // console.log(someCar);
 
-// someCar.__proto__.run
-// someCar
+// console.log(someCar.__proto__);
+
+someCar.run();
+
+// console.log(someCar);
 
 // способы задания прототипов
 // let someObj = {};
@@ -25,30 +30,31 @@
 // console.log(someObj);
 
 // const someObj = Object.create(null);
+// const someObj = new Object(null);
 
-// someObj.hello = 'привет';
+// someObj.hello = "прsивет";
 // console.log(someObj);
 
 // встроенные объекты, полифилы
-// const arr = ['a', 'b', 'c']; // new Array();
-// console.log(arr.join('-'));
+const arr = ["a", "b", "c"]; // new Array();
+// console.log(arr.join("-"));
 // Array.prototype.join = undefined;
+// console.log(arr.join("-"));
 //  смотри join-polyfill.js
 
 // защищенные свойства
-// function User(name) {
-//   let age = 15;
+function User(name) {
+  let age = 15;
 
-//   this.name = name;
+  this.name = name;
 
-//   this.sayHello = function() {
-//     return this.name + ' says hello ' + age;
-//   }
-// }
-// const user = new User('Petya');
-// console.log('user: ', user);
+  this.sayHello = function() {
+    return this.name + " says hello " + age;
+  };
+}
+const user = new User("Petya");
+// console.log("user: ", user);
 // console.log(user.sayHello());
-
 
 // наследование свойств не из прототипа
 
@@ -58,8 +64,8 @@ function Animal(powCount) {
 }
 
 Animal.prototype.run = function() {
-  console.log('rUUUUn');
-}
+  console.log("rUUUUn");
+};
 
 function Cat(name, ...args) {
   // this = {}
@@ -72,16 +78,19 @@ const prototypeConnector = Object.create(Animal.prototype);
 Cat.prototype = prototypeConnector;
 Cat.prototype.constructor = Cat;
 Cat.prototype.run = function() {
-  console.log('cat run');
-}
+  console.log("cat run");
+};
 
 const someAnimal = new Animal(4);
-const someCat = new Cat('barsik', 4);
+const someCat = new Cat("barsik", 4);
 
-// console.log(someAnimal);
-// console.log(someCat.run());
+console.log(someAnimal);
+console.log(Animal.prototype);
 console.log(someCat);
+console.log(someCat.powCount);
 console.log(someCat.run());
+console.log(someCat.__proto__);
+console.log(Cat.prototype);
 
 let someObj = {
   a: 1,
@@ -89,12 +98,12 @@ let someObj = {
   __proto__: {
     b: 2
   }
-}
+};
 
-console.log(someObj);
-console.log(someObj.a);
-console.log(someObj.b);
-someObj.a = 3;
-console.log(someObj.a);
-someObj.b = 4;
-console.log(someObj);
+// console.log(someObj);
+// console.log(someObj.a);
+// console.log(someObj.b);
+// someObj.a = 3;
+// console.log(someObj.a);
+// someObj.b = 4;
+// console.log(someObj);
