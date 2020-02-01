d21946428061ece41b9f2509006fe4bd1feb6b08
Alexander <Eliseev>
2020-02-02 00:02:19 +0300

    added homework6

--- homework/lesson4/app.js
+++ homework/lesson4/app.js
@@ -11,6 +11,30 @@
 // calculate('lemon', 2, { apple: 100, pear: 500, melon: 400, lemon: undefined }); // Извините, товар закончился!
 // calculate('pear', 4, { apple: 100, pear: 500, melon: 400, lemon: undefined }); // 2000
 
+// ========================= Решение задачи 1 ========================= //
+
+function calculate (nameOfProduct, count, someObject) {
+    
+    let result = "";
+
+    for (let key in someObject) {
+        if (nameOfProduct == key) {
+            if (someObject[key] === undefined) {
+                result = "Извините, товар закончился!";
+                return result;
+            } else {
+                result = count * someObject[key];
+                return result;
+            }
+        }
+    }
+    return "Такого товара у нас еще нет!"
+}
+
+console.log(calculate('potato', 1, { apple: 100, pear: 500, melon: 400, lemon: undefined }));
+console.log(calculate('lemon', 2, { apple: 100, pear: 500, melon: 400, lemon: undefined }));
+console.log(calculate('pear', 4, { apple: 100, pear: 500, melon: 400, lemon: undefined }));
+
 // 2)
 // напишите функцию deepClone глубокого клонирования объекта, которая создаёт глубокую копию объекта
 // * - глубокая копия - это значит, что если внутри объекта есть свойства объекты - их нужно тоже склонировать
@@ -21,6 +45,33 @@
 // cloneSomeObj === someObj // false при сравнении копия и первоначальный объект не равны
 // cloneSomeObj.metrics === someObj.metrics // false при сравнении вложенного объекта они тоже не равны
 
+// ========================= Решение задачи 2 ========================= //
+
+let someObj = { name: 'Petya', metrics: { weight: 80, height: 180 } };
+
+function deepClone (someObj) {
+
+let clone = {};
+
+    for (let key in someObj) {
+        const value = someObj[key];
+
+        if (typeof(value) === 'object' && value !== null) {
+            clone[key] = deepClone(value);
+    
+        } else if (value !== undefined) {
+            clone[key] = value;
+        }
+    }
+    return clone;
+}   
+
+let cloneSomeObj = deepClone(someObj);
+console.log(cloneSomeObj);
+console.log(cloneSomeObj === someObj);
+console.log(cloneSomeObj.metrics === someObj.metrics);
+
+
 // 3)
 // напишите функцию merge для объединения объектов НЕ используя встроеный метод Object.assign
 // колличество передаваемых аргументов в функцию НЕ ограничено (вложенные объекты копируются по ссылке)
@@ -28,6 +79,32 @@
 // Например:
 // let unionObject = merge({}, { name: 'Vasya' }, { age: 45 }, { isAdmin: true });
 // unionObject -> { name: 'Vasya', age: 45, isAdmin: true }
+
+// ========================= Решение задачи 3 ========================= //
+
+function merge (...args) {
+
+    let newObject = {};
+
+    args.forEach((element) => {
+        if (Object.keys(element) == "" || typeof(element) !== 'object') {
+            args.splice(args.indexOf(element), 1);
+        }
+    });
+
+    args.forEach((element) => {
+       for (let key in element) {
+           newObject[key] = element[key];
+       }
+    });
+
+    //Object.assign(newObject, args);
+
+    return newObject;
+}
+
+console.log(merge({}, { name: 'Vasya' }, {}, { age: 45 }, { isAdmin: true }, {age: 40}));
+
 
 // 4)
 // Есть объект dog = { name: 'Bobik' };
@@ -40,12 +117,45 @@
 // dog.bark(4); // => "Bobik: bark bark bark bark"
 // dog.bark(); // => "Bobik: bark" если аргумент не передать - метод все равно сработает
 
+// ========================= Решение задачи 4 ========================= //
+
+let dog = {
+    name: 'Bobik',
+    bark: function(count = 1) {
+        let array = [];
+        for (let i = 1; i <= count; i++) {
+            array.push('bark');
+        }
+        return this.name + ": " + array.join(" ");
+    },
+};
+console.log(dog.bark(4));
+
+
 // 5)
 // Есть объект товара item = { label: 'phone', price: 500, currency: '$' };
 // сделайте так, чтобы при преобразовании данного объекта
 // к строке возвращалась строка => "500$",
 // а при преобразовании к числе возвращалось просто 500
 // обратите внимание, что 500 и $ это значения полей самого объекта (если их поменять то это будет учитываться при последующих преобразованиях)
+
+// ========================= Решение задачи 5 ========================= //
+
+let item = {
+    label: 'phone',
+    price: 500,
+    currency: '$',
+
+    [Symbol.toPrimitive](type) {
+      return type === "string" ? this.price + this.currency : this.price;
+    }
+};
+
+console.log(String(item));
+console.log(+item);
+console.log('' + item);
+
+
 
 // 6)
 // напишите конструктор Dog который создает объект со свойствами name, age, breed, weight, height, position, status
@@ -59,3 +169,58 @@
 // dog.down() => Меняет свойство status на строку 'lying';
 //
 // создайте массив с 25 объектами Dog
+
+// ========================= Решение задачи 6 ========================= //
+
+function Dog (name, age, breed, weight, height) {
+    this.name = name;
+    this.age = age;
+    this.breed = breed;
+    this.weight = weight;
+    this.height = height;
+    this.position = 'relax';
+    this.status = 'free';
+
+    this.bark = function() {
+        this.name + ": bark";
+    };
+    this.place = function() {
+        this.position = "place";
+    };
+    this.come = function() {
+        this.position = "here";
+    };
+    this.goOut = function() {
+        this.position = "go out";
+    };
+    this.sit = function() {
+        this.status = "sitting";
+    };
+    this.stand = function() {
+        this.status = "standing";
+    };
+    this.down = function() {
+        this.status = "lying";
+    };
+
+}
+
+function creatPackOfDogs () {
+    let packOfDogs = [];
+
+    for (let i = 0; i < 2; i++) {
+        let name = prompt("Введите имя собаки");
+        let age = prompt("Введите возраст собаки");
+        let breed = prompt("Введите породу собаки");
+        let weight = prompt("Введите вес собаки");
+        let height = prompt("Введите рост собаки");
+
+        let dog = new Dog(name, age , breed, weight, height);
+        packOfDogs.push(dog);
+    }
+    return packOfDogs;
+}
+
+let newPack = creatPackOfDogs();
+console.log(newPack);
+
--- homework/lesson6/app.js
+++ homework/lesson6/app.js
@@ -5,34 +5,96 @@
 // meow - выводит в консоль строку "ИМЯ_КОШКИ: meow"
 // up - меняет состояние кошки на 'standing'
 // down - меняет состояние кошки на 'lying'
-//
-// Например:
-// const cat = new Cat('murzik');
-// console.log(cat.name); // 'murzik'
-// console.log(cat.state); // 'lying'
-// cat.up();
-// console.log(cat.state); // 'standing'
-// cat.down();
-// console.log(cat.state); // 'lying'
-// cat.meow(); // выводит в консоль "murzik: meow"
+
+function Cat(name, state) {
+    this.name = name;
+    this.state = state;
+}
+
+Cat.prototype.meow = function() {
+    console.log(this.name + ": meow");
+}
+Cat.prototype.up = function() {
+    this.state = "standing";
+}
+Cat.prototype.down = function() {
+    this.state = "lying";
+}
+
+const cat = new Cat('murzik', 'lying');
+console.log(cat.name); // 'murzik'
+console.log(cat.state); // 'lying'
+cat.up();
+console.log(cat.state); // 'standing'
+cat.down();
+console.log(cat.state); // 'lying'
+cat.meow(); // выводит в консоль "murzik: meow"
 
 
 // 2
 // Cоздайте объект dictionary в котором будут содержаться переводы слов с английского на русский
 // при отсутсвии перевода в данном объекте - должно возвращаться значение undefined
 //
-// Например:
-// dictionary['hello'] = 'привет';
-// dictionary['good morning'] = 'доброе утро';
-// console.log(dictionary['hello']); // 'привет'
-// console.log(dictionary['good morning']); // 'доброе утро'
-// console.log(dictionary['toString']); // undefined
+const dictionary = {
+    "hello": 'привет',
+    "good morning": 'доброе утро',
+};
+Object.prototype.toString = undefined;
+
+dictionary['hello'] = 'привет';
+dictionary['good morning'] = 'доброе утро';
+console.log(dictionary['hello']); // 'привет'
+console.log(dictionary['good morning']); // 'доброе утро'
+console.log(dictionary['toString']); // undefined
+
 
 // 3
 // Напишите полифил для метода массива forEach
+//alert(Array.prototype.join);
+var fruits = ['Яблоко', 'Банан'];
+//console.log(fruits.join("-"));
+
+Array.prototype.forEach = undefined;
+
+if (!Array.prototype.forEach) {
+	Array.prototype.forEach = function(fn) {
+        //debugger;
+		for(var i = 0, len = this.length; i < len; ++i)
+		{
+            //console.log(this);
+            //console.log(this[i]);
+			fn.call(this, this[i]);
+		}
+	}
+}
+
+fruits.forEach(function(item) {
+    console.log(item);
+});
 
 // 4
 // Напишите полифил для метода массива join
+
+Array.prototype.join = undefined;
+//console.log(fruits.join("-"));
+//alert(!Array.prototype.join);
+
+(function () {
+    if(!Array.prototype.join) {
+      Array.prototype.join = function(separator) {
+        var result = '';
+
+        for (var i = 0; i < this.length; i++) {
+          result += result ? separator + this[i] : this[i];
+        }
+
+        return result;
+      }
+    }
+  })();
+
+  console.log(fruits.join("==="));
+
 
 // 5 (наследование свойст без прототипов)
 // Создайте два конструктора Animal и Dog
@@ -44,18 +106,59 @@
 // так чтобы все экземпляры класса Dog имели свойства { movingType, color, name, age, weight };
 // Например: const someDog = new Dog('walking', 'black', 'tuzik', 4, 10); // { movingType: 'walking', color: 'black', name: 'tuzik', age: 4, weight: 10 };
 
+function Animal(movingType, color) {
+    this.movingType = movingType;
+    this.color = color;
+};
+const catOne = new Animal('walking', 'black');
+const catTwo = new Animal('running', 'white');
+
+function Dog(name, age, weight, ...args) {
+    Animal.apply(this, args);
+    this.name = name;
+    this.age = age;
+    this.weight = weight;
+};
+
+const dogOne = new Dog('tuzik', 4, 10, 'walking', 'black',);
+const dogTwo = new Dog('Teddi', 2, 8, 'running', 'white');
+console.log(dogOne);
+
+
 // 6 (наследование через прототипы)
 // Создайте два конструктора Transport и Car
 // Эклемпляры конструктора Transport имеют свойтсво status и методы run и stop,
 // которые меняют свойсто status на 'running' и 'stopped' соответсвтенно.
 // Унаследуйте эти методы и свойство конструктором Car
-//
-// Например:
-// const someTransport = new Transport();
-// console.log(someTransport.status); // 'stopped'
-// console.log(someTransport.run()); // 'running'
-// console.log(someTransport.stop()); // 'stopped'
-// const someCar = new Car();
-// console.log(someCar.status); // 'stopped'
-// console.log(someCar.run()); // 'running'
-// console.log(someCar.stop()); // 'stopped'
+
+function Transport () {
+};
+Transport.prototype.status = "stopped";
+Transport.prototype.run = function() {
+    return this.status = "running";
+};
+Transport.prototype.stop = function() {
+    return this.status = "stopped";
+};
+
+function Car () {
+    //Transport.apply(this);
+};
+Car.prototype = Object.create(Transport.prototype);
+Car.prototype.constructor = Car;
+
+
+const someTransport = new Transport();
+console.log(Transport.prototype);
+
+console.log(someTransport.status); // 'stopped'
+console.log(someTransport.run()); // 'running'
+console.log(someTransport.stop()); // 'stopped'
+
+const someCar = new Car();
+
+console.log(someCar);
+
+console.log(someCar.status); // 'stopped'
+console.log(someCar.run()); // 'running'
+console.log(someCar.stop()); // 'stopped'
