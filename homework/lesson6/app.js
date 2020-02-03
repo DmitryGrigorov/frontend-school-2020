c1a8ba872d73afc2e30bbcc2a270a4bb6c4a0dae
DolskayaEkaterina <des_ksd@mail.ru>
2020-02-03 16:44:54 +0300

    HW Lesson6   1,2 +

--- homework/lesson6/app.js
+++ homework/lesson6/app.js
@@ -16,6 +16,32 @@
 // console.log(cat.state); // 'lying'
 // cat.meow(); // выводит в консоль "murzik: meow"
 
+/*
+//       Решение.
+
+    const cat = new Cat('Murzik');
+    function Cat(name) {
+       this.name = name;
+       this.state = 'jumping';
+    }
+    Cat.prototype.meow = function() {
+    console.log(`${this.name}: meow`);
+    }
+    Cat.prototype.up = function() {
+       this.state = 'standing';
+    }
+    Cat.prototype.down = function() {
+       this.state = 'lying';
+    }
+
+    console.log(cat.name);       // Murzik
+    console.log(cat.state);      // jumping
+    cat.up();                      
+    console.log(cat.state);      // standing
+    cat.down();
+    console.log(cat.state);      // lying
+    cat.meow();                  // Murzik: meow
+*/
 
 // 2
 // Cоздайте объект dictionary в котором будут содержаться переводы слов с английского на русский
@@ -28,11 +54,29 @@
 // console.log(dictionary['good morning']); // 'доброе утро'
 // console.log(dictionary['toString']); // undefined
 
+/*
+//       Решение.
+
+    let dictionary = Object.create(null);
+       dictionary['hello'] = 'привет';
+       dictionary['good morning'] = 'доброе утро';
+
+    console.log(dictionary['hello']);          // 'привет'
+    console.log(dictionary['good morning']);   // 'доброе утро'
+    console.log(dictionary['toString']);       //  undefined
+*/
+
+    
 // 3
 // Напишите полифил для метода массива forEach
 
+
+
 // 4
 // Напишите полифил для метода массива join
+
+
+
 
 // 5 (наследование свойст без прототипов)
 // Создайте два конструктора Animal и Dog
@@ -43,6 +87,8 @@
 // Реализуйте наследование класса Animal классом Dog,
 // так чтобы все экземпляры класса Dog имели свойства { movingType, color, name, age, weight };
 // Например: const someDog = new Dog('walking', 'black', 'tuzik', 4, 10); // { movingType: 'walking', color: 'black', name: 'tuzik', age: 4, weight: 10 };
+
+
 
 // 6 (наследование через прототипы)
 // Создайте два конструктора Transport и Car
