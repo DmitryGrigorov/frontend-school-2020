d9a13078d9e8999079eb9c201ba44377c678ff24
Andrey Shavyrin <shavyrin1605@gmail.com>
2020-02-03 19:01:34 +0300

    add lesson 6

---
+++ homework/lesson6/app.js
@@ -0,0 +1,99 @@
+// 1
+// Создайте конструктор Cat экземпляры которого (объекты созданные с помощью данного конструктора)
+// должны содержать свойства { name, state }
+// и должны наследовать методы из прототипа
+// meow - выводит в консоль строку "ИМЯ_КОШКИ: meow"
+// up - меняет состояние кошки на 'standing'
+// down - меняет состояние кошки на 'lying'
+//
+// Например:
+// const cat = new Cat('murzik');
+// console.log(cat.name); // 'murzik'
+// console.log(cat.state); // 'lying'
+// cat.up();
+// console.log(cat.state); // 'standing'
+// cat.down();
+// console.log(cat.state); // 'lying'
+// cat.meow(); // выводит в консоль "murzik: meow"
+function Cat(name) {
+    this.name = name;
+    this.state = 'lying';
+}
+
+Cat.prototype.meow = function () {
+    console.log(this.name + ": meow");
+};
+Cat.prototype.up = function () {
+    this.state = 'standing';
+};
+Cat.prototype.down = function () {
+    this.state = 'lying';
+};
+
+
+// 2
+// Cоздайте объект dictionary в котором будут содержаться переводы слов с английского на русский
+// при отсутсвии перевода в данном объекте - должно возвращаться значение undefined
+//
+// Например:
+// dictionary['hello'] = 'привет';
+// dictionary['good morning'] = 'доброе утро';
+// console.log(dictionary['hello']); // 'привет'
+// console.log(dictionary['good morning']); // 'доброе утро'
+// console.log(dictionary['toString']); // undefined
+let dictionary = Object.create(null);
+console.log(dictionary);
+dictionary.hello = 'привет';
+dictionary.cat = 'кошка';
+dictionary.dog = 'собака';
+dictionary.table = 'стол';
+dictionary.box = 'коробка';
+// 3
+// Напишите полифил для метода массива forEach
+(function () {
+    if (!Array.prototype.forEach) {
+        Array.prototype.forEach = function (func) {
+            for (let i = 0; i < this.length; i++) {
+                func(this[i], i, this);
+            }
+        }
+    }
+})();
+// 4
+// Напишите полифил для метода массива join
+(function () {
+    if (!Array.prototype.joi) {
+        Array.prototype.joi = function (separator) {
+            let str = '';
+            for (let i = 0; i < this.length; i++) {
+                str += str ? separator + this[i]: this[i];
+            }
+            return str;
+        }
+    }
+})();
+// 5 (наследование свойст без прототипов)
+// Создайте два конструктора Animal и Dog
+// У всех экземпляров класса Animal есть собственные (не наследуемые) свойства: { movingType, color }
+// Например: const someAnimal = new Animal('walking', 'black'); // { movingType: 'walking', color: 'black' };
+// А у экземпляров класса Dog есть собственные свойства: { name, age, weight }
+// Например: const someDog = new Dog('tuzik', 4, 10); // { name: 'tuzik', age: 4, weight: 10 };
+// Реализуйте наследование класса Animal классом Dog,
+// так чтобы все экземпляры класса Dog имели свойства { movingType, color, name, age, weight };
+// Например: const someDog = new Dog('walking', 'black', 'tuzik', 4, 10); // { movingType: 'walking', color: 'black', name: 'tuzik', age: 4, weight: 10 };
+
+// 6 (наследование через прототипы)
+// Создайте два конструктора Transport и Car
+// Эклемпляры конструктора Transport имеют свойтсво status и методы run и stop,
+// которые меняют свойсто status на 'running' и 'stopped' соответсвтенно.
+// Унаследуйте эти методы и свойство конструктором Car
+//
+// Например:
+// const someTransport = new Transport();
+// console.log(someTransport.status); // 'stopped'
+// console.log(someTransport.run()); // 'running'
+// console.log(someTransport.stop()); // 'stopped'
+// const someCar = new Car();
+// console.log(someCar.status); // 'stopped'
+// console.log(someCar.run()); // 'running'
+// console.log(someCar.stop()); // 'stopped'
