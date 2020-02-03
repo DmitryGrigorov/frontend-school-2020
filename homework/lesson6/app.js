3bb1a3cd2d0cd7bb03c3b00d68e6ad7c882ab5fb
Irina <irinakir98@gmail.com>
2020-02-03 15:24:13 +0300

    Сделано дз #6

--- homework/lesson6/app.js
+++ homework/lesson6/app.js
@@ -16,6 +16,31 @@
 // console.log(cat.state); // 'lying'
 // cat.meow(); // выводит в консоль "murzik: meow"
 
+function Cat(name) {
+    this.name = name;
+    this.state = 'run';
+}
+
+Cat.prototype.meow = function() {
+    console.log(`${this.name}: meow`);
+}
+
+Cat.prototype.up = function() {
+    this.state = 'standing';
+}
+
+Cat.prototype.down = function() {
+    this.state = 'lying';
+}
+
+const cat = new Cat('murzik');
+console.log(cat.name); 
+console.log(cat.state); 
+cat.up();
+console.log(cat.state); 
+cat.down();
+console.log(cat.state); 
+cat.meow(); 
 
 // 2
 // Cоздайте объект dictionary в котором будут содержаться переводы слов с английского на русский
@@ -28,11 +53,59 @@
 // console.log(dictionary['good morning']); // 'доброе утро'
 // console.log(dictionary['toString']); // undefined
 
+let dictionary = Object.create(null);
+
+dictionary['hello'] = 'привет';
+dictionary['good morning'] = 'доброе утро';
+
+console.log(dictionary['hello']); // 'привет'
+console.log(dictionary['good morning']); // 'доброе утро'
+console.log(dictionary['toString']); // undefined
+
 // 3
 // Напишите полифил для метода массива forEach
 
+Array.prototype.forEach = undefined;
+(function () {
+    if(!Array.prototype.forEach) {
+      Array.prototype.forEach = function(func) {
+  
+        for (var i = 0; i < this.length; i++) {
+            func(this[i], i, this) 
+        }
+
+      }
+    }
+})();
+
+let arr = ['a', 'b', 'c', 'd'];
+arr.forEach(function(item, index, mass) {
+    return (console.log(index + ': ' + item +  ': ' + mass));
+});
+
 // 4
 // Напишите полифил для метода массива join
+
+Array.prototype.join = undefined;
+
+(function () {
+    if(!Array.prototype.join) {
+      Array.prototype.join = function(separator) {
+        var result = '';
+  
+        for (var i = 0; i < this.length; i++) {
+            if (result) {
+                result += separator + this[i];
+            } else result += this[i];
+        }
+  
+        return result;
+      }
+    }
+})();
+
+let arr = [1, 2, 3, 4].join(' % ');
+console.log(arr);
 
 // 5 (наследование свойст без прототипов)
 // Создайте два конструктора Animal и Dog
@@ -43,6 +116,23 @@
 // Реализуйте наследование класса Animal классом Dog,
 // так чтобы все экземпляры класса Dog имели свойства { movingType, color, name, age, weight };
 // Например: const someDog = new Dog('walking', 'black', 'tuzik', 4, 10); // { movingType: 'walking', color: 'black', name: 'tuzik', age: 4, weight: 10 };
+
+function Animal(movingType, color) {
+    this.movingType = movingType;
+    this.color = color;
+}
+
+function Dog(name, age, weight, ...args) {
+    Animal.apply(this, args);
+    this.name = name;
+    this.age = age;
+    this.weight = weight;
+}
+
+const someAnimal = new Animal('walking', 'black');
+const someDog = new Dog('tuzik', 4, 10, 'walking', 'black');
+console.log(someAnimal);
+console.log(someDog);
 
 // 6 (наследование через прототипы)
 // Создайте два конструктора Transport и Car
@@ -59,3 +149,38 @@
 // console.log(someCar.status); // 'stopped'
 // console.log(someCar.run()); // 'running'
 // console.log(someCar.stop()); // 'stopped'
+
+function Transport() {
+    this.status = 'ok';
+    this.run = function() {
+        this.status = 'running';
+    };
+    this.stop = function() {
+        this.status = 'stopped';
+    };
+}
+
+function Car() {
+    Transport.apply(this);
+}
+
+//Car.prototype.constructor = Transport; //так тоже работает
+Car.prototype.constructor = Object.create(Transport.prototype);
+
+// const prototypeConnector = Object.create(Transport.prototype);
+// Car.prototype = prototypeConnector;
+// Car.prototype.constructor = Car;
+
+const someTransport = new Transport();
+console.log(someTransport.status); 
+someTransport.run();
+console.log(someTransport.status);
+someTransport.stop();
+console.log(someTransport.status);
+
+const someCar = new Car();
+console.log(someCar.status); 
+someCar.run();
+console.log(someCar.status);
+someCar.stop();
+console.log(someCar.status);
