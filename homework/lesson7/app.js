b03b2412ad5f00e60f6e5fe8deb1fac49595e8ba
Кононов Алексей <mozgoed123@mail.ru>
2020-02-05 01:51:28 +0300

    Lesson 7 - 1) готово

--- homework/lesson7/app.js
+++ homework/lesson7/app.js
@@ -19,37 +19,64 @@
     cat.meow(); // выводит в консоль "murzik: meow"
  */
 
- /*
-    2)
+class Cat {
+    name;
+    state;
+    constructor(name) {
+        this.name = name;
+        this.state = 'lying';
+    }
+    up() {
+        this.state = 'standing';
+    }
+    down() {
+        this.state = 'lying';
+    }
+    meow() {
+        console.log(this.name+': meow');
+    }
+}
+console.log('1)');
+const cat = new Cat('murzik');
+console.log(cat.name); // 'murzik'
+console.log(cat.state); // 'lying'
+cat.up();
+console.log(cat.state); // 'standing'
+cat.down();
+console.log(cat.state); // 'lying'
+cat.meow(); // выводит в консоль "murzik: meow"
 
-    Создайте два класса Animal и Dog
-    У всех экземпляров класса Animal есть собственные (не наследуемые) свойства: { movingType, color }
-    Например: const someAnimal = new Animal('walking', 'black'); // { movingType: 'walking', color: 'black' };
-    А у экземпляров класса Dog есть собственные свойства: { name, age, weight }
-    Например: const someDog = new Dog('tuzik', 4, 10); // { name: 'tuzik', age: 4, weight: 10 };
-    Реализуйте наследование класса Animal классом Dog,
-    так чтобы все экземпляры класса Dog имели свойства { movingType, color, name, age, weight };
-    Например: const someDog = new Dog('walking', 'black', 'tuzik', 4, 10); // { movingType: 'walking', color: 'black', name: 'tuzik', age: 4, weight: 10 };
- */
+/*
+   2)
 
- /*
-    3)
+   Создайте два класса Animal и Dog
+   У всех экземпляров класса Animal есть собственные (не наследуемые) свойства: { movingType, color }
+   Например: const someAnimal = new Animal('walking', 'black'); // { movingType: 'walking', color: 'black' };
+   А у экземпляров класса Dog есть собственные свойства: { name, age, weight }
+   Например: const someDog = new Dog('tuzik', 4, 10); // { name: 'tuzik', age: 4, weight: 10 };
+   Реализуйте наследование класса Animal классом Dog,
+   так чтобы все экземпляры класса Dog имели свойства { movingType, color, name, age, weight };
+   Например: const someDog = new Dog('walking', 'black', 'tuzik', 4, 10); // { movingType: 'walking', color: 'black', name: 'tuzik', age: 4, weight: 10 };
+*/
 
-    Создайте два класса Transport и Car
-    Эклемпляры класса Transport имеют свойтсво status и методы run и stop,
-    которые меняют свойсто status на 'running' и 'stopped' соответсвтенно.
-    Унаследуйте эти методы и свойство классом Car
-    
-    Например:
-    const someTransport = new Transport();
-    console.log(someTransport.status); // 'stopped'
-    console.log(someTransport.run()); // 'running'
-    console.log(someTransport.stop()); // 'stopped'
-    const someCar = new Car();
-    console.log(someCar.status); // 'stopped'
-    console.log(someCar.run()); // 'running'
-    console.log(someCar.stop()); // 'stopped'
- */
+/*
+   3)
+
+   Создайте два конструктора Transport и Car
+   Эклемпляры конструктора Transport имеют свойтсво status и методы run и stop,
+   которые меняют свойсто status на 'running' и 'stopped' соответсвтенно.
+   Унаследуйте эти методы и свойство конструктором Car
+
+   Например:
+   const someTransport = new Transport();
+   console.log(someTransport.status); // 'stopped'
+   console.log(someTransport.run()); // 'running'
+   console.log(someTransport.stop()); // 'stopped'
+   const someCar = new Car();
+   console.log(someCar.status); // 'stopped'
+   console.log(someCar.run()); // 'running'
+   console.log(someCar.stop()); // 'stopped'
+*/
 
 /*
    4)
