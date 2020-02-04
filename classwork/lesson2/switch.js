5a6f02804964e63259166dbf3b5afcced62046b4
aasim313 <ababenko@bk.ru>
2020-02-05 01:10:29 +0300

    homeless6

--- classwork/lesson2/switch.js
+++ classwork/lesson2/switch.js
@@ -1,19 +1,17 @@
 // Напишите if..else, соответствующий следующему switch:
 
 switch (browser) {
-    case 'Edge':
-      alert( "You've got the Edge!" );
-      break;
-  
-    case 'Chrome':
-    case 'Firefox':
-    case 'Safari':
-    case 'Opera':
-      alert( 'Okay we support these browsers too' );
-      break;
-  
-    default:
-      alert( 'We hope that this page looks ok!' );
-  }
+  case "Edge":
+    alert("You've got the Edge!");
+    break;
 
-  
+  case "Chrome":
+  case "Firefox":
+  case "Safari":
+  case "Opera":
+    alert("Okay we support these browsers too");
+    break;
+
+  default:
+    alert("We hope that this page looks ok!");
+}
