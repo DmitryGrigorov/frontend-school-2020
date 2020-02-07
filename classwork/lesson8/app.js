745266dad8699f5131020341e96ee053a05c2178
Andrey Shavyrin <shavyrin1605@gmail.com>
2020-02-07 14:04:06 +0300

    add lesson 7

--- classwork/lesson8/app.js
+++
@@ -1,8 +0,0 @@
-// понятие асинхронного выполнения кода
-// XMLHttpRequest
-// callback hell
-// http://echo.jsontest.com/id/qwerty1
-// понятие промисов
-// решение callback hell
-// синтакчисеский сахар async await
-// fetch
