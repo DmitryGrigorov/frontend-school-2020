68a090939b809f32ab293504f2c76de76eb3b8ec
Kru92 <alekseiy992@gmail.com>
2020-02-08 21:40:37 +0300

    Разбираюсь в гите дальше

--- homework/lesson6/forEach-polyfill.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  if (!Array.prototype.forEach) {
-    Array.prototype.forEach = function(callback) {
-      for (var i = 0; i < this.length; i++) {
-        callback(this[i], i, this);
-      }
-    };
-  }
-})();
