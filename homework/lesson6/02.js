f584b6c29617bd3370aff8199311c435a5329064
Alexander Agafonov <agafonov.alexander@gmail.com>
2020-02-09 19:07:33 +0300

    Lesson 6, task 2

---
+++ homework/lesson6/02.js
@@ -0,0 +1,8 @@
+const dictionary = Object.create(null);
+
+dictionary.hello = "Привет!";
+dictionary.good_morning = "Доброе утро!";
+
+console.log(dictionary['hello']);
+console.log(dictionary['good_morning']);
+console.log(dictionary['toString']);
