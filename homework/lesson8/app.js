caa4c1b4a033f3608361cc4cc36fb30b1e0ae23a
Mozgoed <mozgoed123@mail.ru>
2020-02-09 05:20:07 +0300

    Lesson 8 - всё готово
    Сделано задание 3)

--- homework/lesson8/app.js
+++ homework/lesson8/app.js
@@ -24,11 +24,19 @@
 //   .then(() => delay(3000))
 //   .then(() => console.log('delay callback 3'));
 
-function delay(time) {
-    let promise = new Promise(function(resolve, reject) {
+/**
+ function delay(time) {
+    let promise = new Promise(function (resolve, reject) {
         setTimeout(() => resolve(), time);
     });
     return promise;
+}*/
+
+async function delay(time) {
+    let promise = new Promise((resolve, reject) => {
+        setTimeout(() => resolve(), time)
+    });
+    await promise; // будет ждать, пока промис не выполнится (*)
 }
 
 console.log("Задание №1");
@@ -37,7 +45,8 @@ delay(1000)
     .then(() => delay(2000))
     .then(() => console.log('delay callback 2'))
     .then(() => delay(3000))
-    .then(() => console.log('delay callback 3'));
+    .then(() => console.log('delay callback 3'))
+    .then(function () {
 
 // 2
 // Переделайте запрос на сервер при помощи метода fetch
@@ -63,50 +72,21 @@ delay(1000)
 // request(url)
 //   .then(data => console.log(data)); // выведет в консоль { id: 'qwerty', name: 'petya' }
 
-console.log("Задание №2");
-function request(url) {
-    let promise = new Promise(async function(resolve, reject) {
-        let response = await fetch('http://echo.jsontest.com/id/qwerty/name/petya');
-        let text = await response.text(); // прочитать тело ответа как текст
-        resolve(text);
-    });
-    return promise;
-}
+        console.log("Задание №2");
 
-const someUrl = 'http://echo.jsontest.com/id/qwerty/name/petya';
-request(someUrl)
-    .then(data => console.log(data));
+        async function request(url) {
+            let response = await fetch(url);
+            let text = await response.text(); // прочитать тело ответа как текст
+            let stringText = text.replace('\n', '');
+            return stringText;
+        }
 
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-// ** ПОДСКАЗКА для задачи №2 метод fetch возвращает промис
-// обратите внимание, что после вызова новой функции request в первый же "then" нам попадает уже тело ответа от сервера, а не заголовки ответа
+        const someUrl = 'http://echo.jsontest.com/id/qwerty/name/petya';
+        const someUrl2 = 'http://jsonplaceholder.typicode.com/albums/1';
 
+        request(someUrl2)
+            .then(data => console.log(data))
+            .then(function () {
 
 // 3*
 // Есть массив (колличество элементов может быть любым):
@@ -116,52 +96,29 @@ request(someUrl)
 //     'http://echo.jsontest.com/id/qwerty3/name/kolya'
 // ];
 // загрузите данные со всех урлов параллельно и получите результат в виде массива ответов от сервера [response1, response2, response3]
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
+
+                console.log("Задание №3");
+                let urls = [
+                    'http://jsonplaceholder.typicode.com/albums/1',
+                    'http://jsonplaceholder.typicode.com/albums/2',
+                    'http://jsonplaceholder.typicode.com/albums/3'
+                ];
+                // for (let a = 1; a<500;a++)
+                //     urls.push('http://jsonplaceholder.typicode.com/photos/'+a);
+
+                let mass = [];
+                Promise.all(urls.map(url => {
+                    return fetch(url).then(resp => resp.text());
+                }))
+                    .then(m => {
+                        mass = m;
+                        console.log(mass)
+                    });
+
 // ** ПОДСКАЗКА для задачи №3 с помощью метода массива .map() -> переделайте массив строк в массив промисов
 // во второй задаче есть подстказка как url превращяется в промис
 
+
+            }); //Конец второго задания
+
+    }); //Конец третьего задания
