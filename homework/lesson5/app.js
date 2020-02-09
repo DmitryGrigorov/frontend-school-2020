f932b351cf7604c65445a0ca10218544941fc254
Андрей <gmgm.ab@gmail.com>
2020-02-09 16:52:48 +0300

    added homework lesson5

--- homework/lesson5/app.js
+++ homework/lesson5/app.js
@@ -9,6 +9,13 @@
   sumTo(4) = 4 + 3 + 2 + 1 = 10
 */
 
+function sumTo(n) {
+    if (n === 1) return 1;
+    return n + sumTo(n - 1);
+}
+
+console.log( sumTo(5) );
+
 /*
   2)
   Последовательность чисел Фибоначчи определяется формулой Fn = Fn-1 + Fn-2. То есть, следующее число получается как сумма двух предыдущих.
@@ -26,6 +33,12 @@
   alert(fib(7)); // 13
   alert(fib(77)); // 5527939700884757
 */
+
+function factorial(n) {
+    return (n !== 1) ? n * factorial(n - 1) : 1;
+}
+
+console.log( factorial(2) );
 
 /* 
   3) Следующая функция вычисляет сумму передаваемых ей аргументов,
@@ -50,12 +63,31 @@
   Используя rest-операторы, перепишите эту функцию так, чтобы не использовать arguments.
 */
 
+function sum(...args) {
+    let sumAll = 0;
+    for (let arg of args) sumAll += arg;
+    return sumAll;
+}
+
+console.log(sum(1, 2, 3));
+
 /*
   4) Напишите функцию add, которая работает, как на примере:
 
   add(2, 5) // 7
   add(2)(5) // 7
 */
+
+function add(a, b) {
+
+    if (b) return a + b;
+
+    return function(b) {
+        return a + b;
+    };
+}
+
+console.log(add(2)(5));
 
 /*
   5)
@@ -64,6 +96,16 @@
   // Examples:
   sort([9, 8, 7, 6, 5, 4, 3, 2, 1]) // [9, 2, 7, 4, 5, 6, 3, 8, 1]
 */
+
+function sortOdd(arr) {
+    const oddArr = arr.filter(item => item % 2 === 0).sort((a, b) => a - b);
+
+    let i = 0;
+
+    return arr.map(index => (index % 2 === 0) ? oddArr[i++] : index)
+}
+
+console.log(sortOdd([9, 8, 7, 6, 5, 4, 3, 2, 1]));
 
 /*
   6)
@@ -74,6 +116,12 @@
   findMax(0, 2, 12, 4, 5) // 12
 */
 
+function findMax(...args) {
+    return Math.max.apply(null, args);
+}
+
+console.log(findMax(128, 7, 6, 5, 130, 3, 2, 1));
+
 /*
   7)
   Написать функцию findMin(), которая найдет минимальный элемент из аргументов:
@@ -83,6 +131,11 @@
   findMax(0, 2, 12, 4, 5) // 0
 */
 
+function findMin(...args) {
+    return Math.min.apply(null, args);
+}
+
+console.log(findMin(0, -1, 12, 4, 5));
 
 /*
   8)
@@ -92,3 +145,15 @@
   pushZeros([0, 9, 0, 8, 7, 0, 6, 5, 4, 3, 2, 1]) // [9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 0, 0]
   pushZeros([0, 2, 12, 4, 5]) // [2, 12, 4, 5, 0]
 */
+
+function pushZeros(array) {
+    for (let i = array.length - 1; i>=0; i--) {
+        if(array[i] === 0) {
+            array.splice(i, 1);
+            array.push(0)
+        }
+    }
+    return array;
+}
+
+console.log(pushZeros([0, 0, 0, 1]));
