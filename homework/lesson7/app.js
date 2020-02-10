6201b31af1aee16a777635a0bbef00be6b2a7e7d
Dmitry Razorenov <razorenov.dmitry@yandex.ru>
2020-02-10 13:20:26 +0300

    Lesson 7 is ready

--- homework/lesson7/app.js
+++ homework/lesson7/app.js
@@ -18,23 +18,34 @@
     console.log(cat.state); // 'lying'
     cat.meow(); // выводит в консоль "murzik: meow"
  */
-class Cat{
-   constructor(name){
-      this.name = name;
-      this.state = 'lying';
-   }
+class Cat {
+    constructor(name) {
+        this.name = name;
+        this.state = 'lying';
+    }
+    up() {
+        this.state = 'standing';
+    }
+    
+    down() {
+        this.state = 'lying';
+    }
+    
+    meow() {
+        console.log(this.name + ': meow');
+    }
 }
-Cat.prototype.up = function (){
-   this.state = 'standing';
-}
+// Cat.prototype.up = function () {
+//     this.state = 'standing';
+// }
 
-Cat.prototype.down = function (){
-   this.state = 'lying';
-}
+// Cat.prototype.down = function () {
+//     this.state = 'lying';
+// }
 
-Cat.prototype.meow = function (){
-   console.log(this.name + ': meow');
-}
+// Cat.prototype.meow = function () {
+//     console.log(this.name + ': meow');
+// }
 
 // const cat = new Cat('murzik');
 // console.log(cat.name); // 'murzik'
@@ -44,7 +55,7 @@ Cat.prototype.meow = function (){
 // cat.down();
 // console.log(cat.state); // 'lying'
 // cat.meow(); // выводит в консоль "murzik: meow"
- /*
+/*
     2)
 
     Создайте два класса Animal и Dog
@@ -56,47 +67,47 @@ Cat.prototype.meow = function (){
     так чтобы все экземпляры класса Dog имели свойства { movingType, color, name, age, weight };
     Например: const someDog = new Dog('walking', 'black', 'tuzik', 4, 10); // { movingType: 'walking', color: 'black', name: 'tuzik', age: 4, weight: 10 };
  */
-class Animal{
-   constructor(movingType, color){
-      this.movingType = movingType;
-      this.color = color;
-   }
+class Animal {
+    constructor(movingType, color) {
+        this.movingType = movingType;
+        this.color = color;
+    }
 }
 
-class Dog{
-   constructor(name,  age, weight){
-      this.name = name;
-      this.age = age;
-      this.weight = weight;
-  }
+class Dog {
+    constructor(name, age, weight) {
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+    }
 }
 
-class Dog2 extends Animal{
-   constructor(movingType, color, name,  age, weight){
-      super(movingType, color);
+class Dog2 extends Animal {
+    constructor(movingType, color, name, age, weight) {
+        super(movingType, color);
 
-      this.name = name;
-      this.age = age;
-      this.weight = weight;
-  }
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+    }
 }
 
 const someAnimal = new Animal('walking', 'black');
 const someDog = new Dog('tuzik', 4, 10);
 const someDog2 = new Dog2('walking', 'black', 'tuzik', 4, 10);
 
-// console.log(someAnimal);
-// console.log(someDog);
+// console.log(someAnimal); 
+// console.log(someDog); 
 // console.log(someDog2);
 
- /*
+/*
     3)
 
     Создайте два конструктора Transport и Car
     Эклемпляры конструктора Transport имеют свойтсво status и методы run и stop,
     которые меняют свойсто status на 'running' и 'stopped' соответсвтенно.
     Унаследуйте эти методы и свойство конструктором Car
-    
+
     Например:
     const someTransport = new Transport();
     console.log(someTransport.status); // 'stopped'
@@ -107,23 +118,22 @@ const someDog2 = new Dog2('walking', 'black', 'tuzik', 4, 10);
     console.log(someCar.run()); // 'running'
     console.log(someCar.stop()); // 'stopped'
  */
-class Transport{
-   constructor(){
-      this.status = 'stopped';
-   }
-   run(){
-      return this.status = 'running';
-   }
-   stop(){
-      return this.status = 'stopped';
-   }
+class Transport {
+    constructor() {
+        this.status = 'stopped';
+    }
+    run() {
+        return this.status = 'running';
+    }
+    stop() {
+        return this.status = 'stopped';
+    }
 }
 
-
-class Car extends Transport{
-   constructor(){
-      super();
-   }
+class Car extends Transport {
+    constructor() {
+        super();
+    }
 }
 
 // const someTransport = new Transport();
@@ -161,50 +171,93 @@ class Car extends Transport{
      или росту (по убыванию роста). Вызовите только функцию сортировки по росту.
 */
 
-class Human{
-   constructor(name, age, height){
-      this.name   = name;
-      this.age    = age;
-      this.height = height;
-   }
+class Human {
+    constructor(name, age, height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
 
-   getInfo(){
-      return this.name + ', ' + this.age + ', ' + this.height; 
-   }
+    getInfo() {
+        return this.name + ', ' + this.age + ', ' + this.height;
+    }
 
-   get firstname(){
-      return this.name;
-   }
+    static compareHeight(a, b) {
+        return b.height - a.height;
+    }
 
-   set firstname(name){
-      return this.name = name;
-   }
+    static compareName(a, b) {
+        if (a.name > b.name) {
+            return 1;
+        }
+        if ( b.name > a.name) {
+            return -1;
+        }
+        return 0;
+    }
+
+    get firstname() {
+        return this.name;
+    }
+
+    set firstname(name) {
+        return this.name = name;
+    }
 }
 
 let arr = [
-   'Коля', 23, 180,
-   'Даша', 19, 170,
-   'Ваня', 18, 192,
-   'Петя', 45, 178,
-   'Вася', 34, 197,
-   'Джони', 40, 168,
-   'Катя', 37, 160,
-   'Петя', 29, 200,
-   'Соня', 21, 172,
-   'Женя', 25, 175
-]
+    'Коля',
+    23,
+    180,
+    'Даша',
+    19,
+    170,
+    'Ваня',
+    18,
+    192,
+    'Петя',
+    45,
+    178,
+    'Вася',
+    34,
+    197,
+    'Джони',
+    40,
+    168,
+    'Катя',
+    37,
+    160,
+    'Петя',
+    29,
+    200,
+    'Соня',
+    21,
+    172,
+    'Женя',
+    25,
+    175
+];
 let humans = new Array;
 let i = 0;
-do{
-   humans.push(new Human(arr[i], arr[i+1], arr[i+2]));
-   i=i+3;
-}while(i<arr.length);
+do {
+    humans.push(new Human(arr[i], arr[i + 1], arr[i + 2]));
+    i = i + 3;
+} while (i < arr.length);
 
+// console.log(humans); 
+// console.log(humans[0].firstname); 
+// humans[0].firstname = 'Петя'; 
+// console.log(humans[0].firstname); 
+// console.log(humans[0].getInfo());
+
+function sortByHeight(arr) {
+   return arr.sort(Human.compareHeight);
+}
+
+function sortByName(arr) {
+   return arr.sort(Human.compareName);
+}
+
+
+humans  =  sortByName(humans);
 // console.log(humans);
-console.log(humans[0].firstname);
-humans[0].firstname = 'Петя';
-console.log(humans[0].firstname);
-console.log(humans[0].getInfo());
-
-
-
