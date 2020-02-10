cf7f7d86ea9e5f826ebbba8ccbd6f3fa3a62c829
@anninwonderland <asrakina@miem.hse.ru>
2020-02-10 19:07:59 +0300

    Add ex. 1

--- homework/lesson8/app.js
+++ homework/lesson8/app.js
@@ -63,7 +63,13 @@ function delay(time) {
     return promise;
 }
 
-delay(2000).then(() => console.info('resolved'))
+
+delay(1000)
+  .then(() => console.log('delay callback 1'))
+  .then(() => delay(2000))
+  .then(() => console.log('delay callback 2'))
+  .then(() => delay(3000))
+  .then(() => console.log('delay callback 3'));
 
 
 // 2
