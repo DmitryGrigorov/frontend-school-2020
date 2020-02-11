0e5cf6d1ad446e5707500847fc58a0bab95a371a
eglazkov <egorglazkov@mail.ru>
2020-02-11 20:36:31 +0300

    ДЗ по восьмому уроку. Промисы.

--- homework/lesson8/app.js
+++ homework/lesson8/app.js
@@ -23,6 +23,13 @@
 //   .then(() => console.log('delay callback 2'))
 //   .then(() => delay(3000))
 //   .then(() => console.log('delay callback 3'));
+
+function delay (time) {
+    return delayPromise = new Promise((resolve) => {
+        setTimeout(() => resolve(), time);
+    });
+}
+
 //
 //
 //
@@ -78,6 +85,15 @@
 // запрос на сервер после доработки функции request:
 // request(url)
 //   .then(data => console.log(data)); // выведет в консоль { id: 'qwerty', name: 'petya' }
+
+function request (url) {
+    return new Promise((resolve, reject) => {
+        fetch(url)
+        .then((response) => resolve(response.json()))
+        .catch((e) => reject(e));
+    });
+}
+
 //
 //
 //
@@ -123,6 +139,22 @@
 //     'http://echo.jsontest.com/id/qwerty3/name/kolya'
 // ];
 // загрузите данные со всех урлов параллельно и получите результат в виде массива ответов от сервера [response1, response2, response3]
+
+let urls = [
+    'http://echo.jsontest.com/id/qwerty1/name/petya',
+    'http://echo.jsontest.com/id/qwerty2/name/vasya',
+    'http://echo.jsontest.com/id/qwerty3/name/kolya'
+];
+
+Promise.all(urls.map((url) => {
+    return new Promise((resolve, reject) => {
+        fetch(url)
+        .then((response) => resolve(response.json()))
+        .catch((e) => reject(e));
+    });
+}))
+.then((serverData) => console.log(serverData));
+
 //
 //
 //
