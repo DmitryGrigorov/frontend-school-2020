b33f9ebd749dc36bf053ea3a2d6b4dcc1f3fe0bd
tanyazhd <zhdanovatanya96@gmail.com>
2020-02-12 16:02:49 +0300

    lesson 8 hw

---
+++ homework/lesson8/app.js
@@ -0,0 +1,97 @@
+// 1
+// Есть функция "отложенного вызова функции" delay
+// function delay(time, callback) {
+//     setTimeout(callback, time);
+// }
+//
+// delay(1000, function() {
+//   console.log('delay callback 1');
+//
+//   delay(2000, function() {
+//     console.log('delay callback 2');
+//
+//     delay(3000, function() {
+//       console.log('delay callback 3');
+//     });
+//   });
+// });
+
+
+function delay(time){
+    let promise = new Promise(function(resolve, reject){
+        setTimeout(function(){
+            resolve();
+        },time);
+    });
+    return promise;
+}
+// перепишите её с помощью промисов, чтобы мы могли код выше заменить на:
+delay(1000)
+  .then(() => console.log('delay callback 1'))
+  .then(() => delay(2000))
+  .then(() => console.log('delay callback 2'))
+  .then(() => delay(3000))
+  .then(() => console.log('delay callback 3'));
+
+//  ** ПОДСКАЗКА для задачи №1 обратите внимание, что после вызова delay мы сразу пишем .then, (delay должна вернуть promise)
+
+
+
+// 2
+// Переделайте запрос на сервер при помощи метода fetch
+//
+const someUrl = 'http://echo.jsontest.com/id/qwerty/name/petya';
+const errorUrl = 'hhhh';
+//
+// старая функция для запросов на сервер:
+// function request(url, onloadCallback) {
+//   const xhr = new XMLHttpRequest();
+//   xhr.open('GET', url);
+//   xhr.send();
+//   xhr.onload = function() {
+//     onloadCallback(xhr.response);
+//   }
+// }
+//
+// старый запрос на сервер:
+// request(someUrl, function (response) {
+//   console.log(response); // выведет в консоль { id: 'qwerty', name: 'petya' }
+// });
+
+
+function requestNew(url){
+    return fetch(url)
+    .then(data => (data.ok) ? data.text() : Promise.reject(data.status))
+    .catch((error) => {
+        console.log('ERROR ' + error);
+    });
+}
+// запрос на сервер после доработки функции request:
+requestNew(someUrl)
+  .then(data =>  console.log(data)); // выведет в консоль { id: 'qwerty', name: 'petya' }
+
+// ** ПОДСКАЗКА для задачи №2 метод fetch возвращает промис
+// обратите внимание, что после вызова новой функции request в первый же "then" нам попадает уже тело ответа от сервера, а не заголовки ответа
+
+
+// 3*
+// Есть массив (колличество элементов может быть любым):
+let urls = [
+    'http://echo.jsontest.com/id/qwerty1/name/petya',
+    'http://echo.jsontest.com/id/qwerty2/name/vasya',
+    'http://echo.jsontest.com/id/qwerty3/name/kolya'
+];
+// загрузите данные со всех урлов параллельно и получите результат в виде массива ответов от сервера [response1, response2, response3]
+
+// let responses = urls.map(async function(url){
+//     return fetch(url)
+// });
+// Promise.all(responses)
+//     .then(responses => console.log(responses));
+
+Promise.all(urls.map(url => fetch(url)))
+    .then(data=>console.log(data));
+
+// ** ПОДСКАЗКА для задачи №3 с помощью метода массива .map() -> переделайте массив строк в массив промисов
+// во второй задаче есть подстказка как url превращяется в промис
+
