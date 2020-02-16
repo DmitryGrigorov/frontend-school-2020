f00a7aae0f032cc712e4b7fe90850853dac495bb
karelalex <karelalex83@gmail.com>
2020-02-16 22:41:54 +0300

    ДЗ 8 сделано

--- homework/lesson8/app.js
+++ homework/lesson8/app.js
@@ -17,12 +17,18 @@
 // });
 //
 // перепишите её с помощью промисов, чтобы мы могли код выше заменить на:
-// delay(1000)
-//   .then(() => console.log('delay callback 1'))
-//   .then(() => delay(2000))
-//   .then(() => console.log('delay callback 2'))
-//   .then(() => delay(3000))
-//   .then(() => console.log('delay callback 3'));
+
+const delay = (ms) => {
+    return new Promise(((resolve, reject) => {
+        setTimeout(resolve, ms);
+    }))
+}
+delay(1000)
+  .then(() => console.log('delay callback 1'))
+  .then(() => delay(2000))
+  .then(() => console.log('delay callback 2'))
+  .then(() => delay(3000))
+  .then(() => console.log('delay callback 3'));
 //
 //
 //
@@ -79,6 +85,18 @@
 // request(url)
 //   .then(data => console.log(data)); // выведет в консоль { id: 'qwerty', name: 'petya' }
 //
+const someUrl = 'http://echo.jsontest.com/id/qwerty/name/petya';
+const   request = async (url) => {
+    const t = await fetch(url, {mode: "no-cors"})
+    if(t.ok) {
+        return t.json();
+    }
+    else {
+        alert("Ошибка HTTP: " + t.status);
+        return Promise.reject('connection troubles');
+    }
+}
+request(someUrl).then(data => console.log(data)); // выведет в консоль { id: 'qwerty', name: 'petya' }
 //
 //
 //
@@ -117,15 +135,18 @@
 
 // 3*
 // Есть массив (колличество элементов может быть любым):
-// let urls = [
-//     'http://echo.jsontest.com/id/qwerty1/name/petya',
-//     'http://echo.jsontest.com/id/qwerty2/name/vasya',
-//     'http://echo.jsontest.com/id/qwerty3/name/kolya'
-// ];
+let urls = [
+    'http://echo.jsontest.com/id/qwerty1/name/petya',
+    'http://echo.jsontest.com/id/qwerty2/name/vasya',
+    'http://echo.jsontest.com/id/qwerty3/name/kolya'
+];
 // загрузите данные со всех урлов параллельно и получите результат в виде массива ответов от сервера [response1, response2, response3]
 //
 //
-//
+const results = Promise.all(urls.map((item) =>request(item)));
+results.then((resArr) => {
+    resArr.forEach(data => console.log(data)); //тот самый массив из задания.
+})
 //
 //
 //
