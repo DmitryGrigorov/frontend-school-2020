86628d23922f6fc218566179dd1a636758afca3c
Olga Serebryakova <lelka-7770@yandex.ru>
2020-02-18 10:51:07 +0300

    lesson11

--- classwork/lesson11/3_drag_and_drop/app.js
+++ classwork/lesson11/3_drag_and_drop/app.js
@@ -6,16 +6,23 @@ ball.ondragstart = function() {
 };
 
 // mousedown -> mousemove -> mouseup
-ball.addEventListener('mousedown', function(event) { // (1) отследить нажатие
+ball.addEventListener('mousedown', function(event) { 
+  // (1) отследить нажатие
   // (2) подготовить к перемещению:
   // разместить поверх остального содержимого и в абсолютных координатах
-  
+  ball.style.position = "absolute";
+  ball.style.zIndex = 1000;
   // переместим в body, чтобы мяч был точно не внутри position:relative
-  
+  document.body.append(ball);
   
   // и установим абсолютно спозиционированный мяч под курсор
+  moveAt(event.pageX, event.pageY);
 
   // moveAt
+  function moveAt(pageX, pageY) {
+    ball.style.left = pageX + 'px';
+    ball.style.top = pageY + 'px'
+  }
   // передвинуть мяч под координаты курсора
   // и сдвинуть на половину ширины/высоты для центрирования
 
