944cc35425cf816b3f8bd16c842f3a4249b66edf
DolskayaEkaterina <des_ksd@mail.ru>
2020-02-19 17:02:55 +0300

    HW Lesson 8 +

--- homework/lesson8/app.js
+++ homework/lesson8/app.js
@@ -3,19 +3,19 @@
 // function delay(time, callback) {
 //     setTimeout(callback, time);
 // }
-//
+
 // delay(1000, function() {
 //   console.log('delay callback 1');
-//
+
 //   delay(2000, function() {
 //     console.log('delay callback 2');
-//
+
 //     delay(3000, function() {
 //       console.log('delay callback 3');
 //     });
 //   });
 // });
-//
+
 // перепишите её с помощью промисов, чтобы мы могли код выше заменить на:
 // delay(1000)
 //   .then(() => console.log('delay callback 1'))
@@ -23,36 +23,28 @@
 //   .then(() => console.log('delay callback 2'))
 //   .then(() => delay(3000))
 //   .then(() => console.log('delay callback 3'));
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
+// 
 //  ** ПОДСКАЗКА для задачи №1 обратите внимание, что после вызова delay мы сразу пишем .then, (delay должна вернуть promise)
 
+
+ 
+// //       Решение.
+
+//     function delay (time) {
+//         return new Promise ((resolve) => {
+//             setTimeout(() => resolve(), time);
+//         });
+//     }
+
+//     delay(1000)
+//        .then(() => console.log('delay callback 1'))
+//        .then(() => delay(2000))
+//        .then(() => console.log('delay callback 2'))
+//        .then(() => delay(3000))
+//        .then(() => console.log('delay callback 3'));
+
+
+//_______________________________________________________________________________
 
 
 // 2
@@ -79,40 +71,42 @@
 // request(url)
 //   .then(data => console.log(data)); // выведет в консоль { id: 'qwerty', name: 'petya' }
 //
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
 // ** ПОДСКАЗКА для задачи №2 метод fetch возвращает промис
 // обратите внимание, что после вызова новой функции request в первый же "then" нам попадает уже тело ответа от сервера, а не заголовки ответа
+//
+
+
+
+// //       Для собственного понимания (без учета нового запроса из условия задачи дз) 
+//     fetch('http://echo.jsontest.com/id/qwerty/name/petya')
+    
+//         .then ((response) => {
+//            return response.text();    // response.text() возвращает новый промис, который выполняется и возвращает полный ответ сервера
+//         })
+//         .then ((text) => {            // содержимое полученного файла
+//             console.log(text);        // { id: 'qwerty', name: 'petya' }
+//         });
+
+
+
+// //          Решение. 
+
+//         const someUrl = 'http://echo.jsontest.com/id/qwerty/name/petya';
+
+//         function request (someUrl) {
+//             return fetch(someUrl)
+//                 .then((response) => {
+//                     return response.text();  // при использовании response.json() в консоль выводится Object с содержимым  { id: 'qwerty', name: 'petya' } 
+//                 })
+//         }
+//         request(someUrl)
+//             .then((data) => { 
+//                 console.log(data); 
+//             });                              // выведет в консоль { id: 'qwerty', name: 'petya' }
+
+
+//___________________________________________________________________________________
+
 
 
 // 3*
@@ -125,50 +119,26 @@
 // загрузите данные со всех урлов параллельно и получите результат в виде массива ответов от сервера [response1, response2, response3]
 //
 //
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
 // ** ПОДСКАЗКА для задачи №3 с помощью метода массива .map() -> переделайте массив строк в массив промисов
 // во второй задаче есть подстказка как url превращяется в промис
 
+
+
+// //          Решение.
+
+//         let urls = [
+//             'http://echo.jsontest.com/id/qwerty1/name/petya',
+//             'http://echo.jsontest.com/id/qwerty2/name/vasya',
+//             'http://echo.jsontest.com/id/qwerty3/name/kolya'
+//         ];
+
+//         Promise.all (urls.map ((url) => {
+//             return fetch (url) 
+//                 .then ((response) => {
+//                    return response.text();
+//                 });
+//         }   
+//         ))
+//             .then ((data) => {
+//                 console.log(data);
+//             })
