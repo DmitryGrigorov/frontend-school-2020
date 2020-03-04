25d58cd0c71ca9406c31365732ae8fa4ec1e60ce
Григоров Дмитрий <dmitriy.v.grigorov@gazprombank.ru>
2020-03-04 20:16:58 +0300

    example toggle

--- classwork/lesson15/classswork-project/src/components/app.jsx
+++ classwork/lesson15/classswork-project/src/components/app.jsx
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 
+import Counter from './counter';
+
 class App extends Component {
   state = {
-    counter: 0
+    counter: 0,
+    isHide: false
   };
 
   incrementHandler = () => {
     this.setState({
-      counter: this.state.counter + 1
+      counter: this.state.counter + 1,
     })
   };
 
@@ -17,12 +20,19 @@ class App extends Component {
     })
   };
 
+  toggleHandler = () => {
+    this.setState({
+      isHide: !this.state.isHide
+    });
+  };
+
   render() {
     return (
       <div>
         <button onClick={this.incrementHandler}>Increment</button>
         <button onClick={this.decrementHandler}>Decrement</button>
-        <h1>{this.state.counter}</h1>
+        <button onClick={this.toggleHandler}>Toggle</button>
+        { this.state.isHide && <Counter counter={this.state.counter} /> }
       </div>
     );
   }
