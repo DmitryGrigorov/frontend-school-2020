836f644ffdf1258062e866341d605b41983cc12b
tanyazhd <zhdanovatanya96@gmail.com>
2020-04-05 19:19:18 +0300

    delete files

--- classwork/lesson13/node-example/index.js
+++
@@ -1,8 +0,0 @@
-const {greeting, func2} = require('./module');
-
-const a = 5; 
-const b = 3;
-
-console.log(a+b)
-greeting();
-func2()
--- classwork/lesson14/simple-spa/src/index.js
+++
@@ -1,5 +0,0 @@
-import Application from './application';
-
-const root = document.getElementById('root');
-const app = new Application();
-root.append(app.element);
