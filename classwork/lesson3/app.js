e547979869c077b7db25b45fc633f6918a217a7d
Dmitry Grigorov <d.v.grigorov@gmail.com>
2021-08-05 11:27:30 +0300

    update classwork

--- classwork/lesson3/app.js
+++ classwork/lesson3/app.js
@@ -1,7 +1,156 @@
 // Strings — Строки
 
-// let str1 = 'Some string';
-// let str2 = "Some string";
+// let str1 = 'Some \' string';
+// let str2 = "Some \u00A9 string";
+
+// let a = 234;
+// let b = 323;
+// let str3 = `${a} + ${b} =
+
+// ${a + b}`;
 
 
+// const AAA = 'ABCsdfjsldfjlsd';
+
+// console.log(AAA[0]);
+// console.log('charAt: ', AAA.charAt(1))
+
+// AAA.split('').forEach((el, idx) => {
+//     console.log({ el, idx })
+// });
+
+
+// const str = AAA[0] + AAA[1] + AAA[2];
+
+// console.log(str);
+
+// toUpperCase
+// console.log(AAA.toUpperCase());
+
+// toLowerCase
+// console.log(AAA.toLowerCase());
+
+// console.log(AAA[0].toLowerCase());
+
+// const str12 = "Widget with id";
+
+// console.log(str12.indexOf("widget"));
+
+// console.log(str12.includes('widget'))
+
+// console.log(str12.startsWith('Widget'));
+
+// console.log(str12.endsWith('id'));
+
+// const repeatedStr = 'A';
+
+// console.log(repeatedStr.repeat(4));
+
+// Массивы
+
+// let empty = [];
+// let primes = [1, 2,3,4,5];
+// let mix = [12, true, "a", {}];
+
+// let sumArray = [primes, primes[4], [5, , 5, 5] ];
+
+// console.log(sumArray);
+
+// let arr = new Array(5);
+
+// console.log(arr.fill(0));
+
+// let str = '12345';
+
+// console.log(str.split(''))
+
+// let numbers = [1,2,3,4,5];
+
+// console.log(numbers.join('').split('').reverse().join(''));
+
+// console.log(numbers.reverse());
+
+// function isPalindrome(str) {
+//     return str === str.split('').reverse().join('');
+// }
+
+// console.log(isPalindrome('asdds'));
+// console.log(isPalindrome('racecar'));
+
+// anna
+// racecar
+
+
+// const arr1 = [1, 2, 3];
+
+// const arr2 = [4, 5, 6];
+
+// console.log([...arr1, ...arr2]); // ES6
+// console.log(arr1.concat(arr2));
+
+// const arr = [];
+
+// console.log(arr);
+
+// arr.push(1 , 2);
+
+// console.log(arr); // [1, 2]
+
+// console.log(arr.pop()) // 2
+
+// console.log(arr) // [1]
+
+
+// let arr2 = [];
+
+// arr2.unshift(1); // [1]
+// arr2.unshift(22); // [22, 1]
+
+// arr2.push(5);
+
+// console.log(arr2); // [22, 1, 5]
+
+// arr2.pop(); // [22, 1]
+
+// console.log(arr2);
+
+// arr2.shift(); // 22
+
+// console.log(arr2) // 1
+
+// forEach, map
+
+const order = [1, 2, 3, 4, 5];
+
+const forEach = order.forEach((el, index, arr) => {
+    console.log({
+        el,
+        index,
+        arr
+    });
+});
+
+console.log(forEach); // undefined
+
+let map = order.map((el, index, arr) => {
+    console.log({
+        el,
+        index,
+        arr
+    })
+
+    return el + 2;
+});
+
+// console.log(map) // [3, 4, 5, 6, 7]
+
+// console.log(map.slice(0, 4))
+
+const slice4 = map.slice(0, 4);
+
+slice4.forEach(el => {
+    console.log(el + ' $')
+})
+
+console.log(slice4.find((el) => el % 2));
 
--- classwork/lesson4/app.js
+++ classwork/lesson4/app.js
@@ -1,393 +1,119 @@
-// создание объектов, добавление/удаление свойств *
-// let someObject = new Object();
+// Оюъекты.
 
-// let user = {
-//     prop1: 100,
-//     prop2: 'hello',
-//     return: 'wqr'
-// };
+let empty = {};
 
-// user.name = 'Nick';
+let point = { x: 0, y: 0 };
 
-// delete user.prop;
+let p2 = { x: point.x, y: point.y };
 
-// console.log(user.asdfasd);
+const { x, y } = point;
+
+let p3 = { someX: x, y };
+
+// console.log(p3);
+
+let book = {
+    "main title": "Javascript",
+    "sub-title": "The Definitive Guide",
+    author: {
+        firstname: "David",
+        lastname: "Flanagan"
+    },
+
+    for: "all"
+};
+
+// console.log(book["main title"]);
+
+// console.log(book.author);
+
+let o = new Object();
+let a = new Array();
+let d = new Date();
 
 
-// имя свойства из нескольких слов, квадратные скобки
-
-// let car = {
-//     'max speed': 200
-// };
-
-// let key1 = 'max ';
-// let key2 = 'speed';
-
-// console.log(car[key1 + key2]);
-
-// вычисляемые свойства[], свойства из переменных
-
-// let key1 = 'current';
-// let key2 = 'Speed';
-// let car2 = {
-//     [key1 + key2]: 123
-// };
-
-// car2['some' + 'Prop'] = 'hello world';
-
-// console.log(car2);
-
-// проверка существования свойств
-
-// let item = {
-//     label: 'orange',
-//     price: 500,
-//     age: undefined
-// };
-
-// console.log(item.label);
-
-// console.log(item.age === undefined);
-// console.log('some' + 'Prop' in item);
-
-// цикл for..in
-
-// let someObject = {
-//     '2345': 'werty',
-//     someProp2: 500
-// };
-
-// for(let key in someObject) {
-//     console.log(key, ': ', someObject[key]);
-// }
-
-// копирование по ссылке
-
-//  let str1 = 'hello world';
-//  let str2 = str1;
-
-//  str1 = str1 + '!!!!!!!';
-
-// console.log(str1);
-// console.log(str2);
-
-// let car1 = {
-//     model: 'bmw',
-//     color: 'black'
-// };
-
-// let car2 = car1;
-
-// car1.speed = 100;
-
-// console.log(car2);
-
-// сравнение объектов
-
-// let obj1 = {};
-// let obj2 = {};
-// let obj3 = obj1;
-
-// console.log(obj1 == obj1);
-
-// клонирование и объединение объектов
-
-// function clone(someObj) {
-//     let cloneObj = {};
-
-//     for (let prop in someObj) {
-//         cloneObj[prop] = someObj[prop];
-//     }
-
-//     return cloneObj;
-// }
-
-// let car1 = {
-//     model: 'renault',
-//     color: 'blue'
-// };
-
-// let cloneCar1 = clone(car1);
-
-// car1.speed = 40;
-
-// console.log(car1);
-// console.log(cloneCar1);
-
-// const metrics = {
-//     height: 120,
-//     weight: 140
-// };
-
-// let user = {
-//     name: 'Vova',
-//     metrics
-// }
-
-// let cloneUser = clone(user);
-
-// console.log(user);
-// console.log(cloneUser);
-
-// function filter(obj) {
-//     for (let key in obj) {
-//         if(obj[key] === undefined) {
-//             delete obj[key];
-//         }
-//     }
-// }
-
-// function filterWithClone(obj) {
-//     let clone = {};
-
-//     for (let key in obj) {
-//         const value = obj[key];
-
-//         if (typeof(value) === 'object' && value !== null) {
-//             clone[key] = filterWithClone(value);
-//         } else if (value !== undefined) {
-//             clone[key] = value;
-//         }
-//     }
-
-//     return clone;
-// }
-
-// let car = {
-//     color: undefined,
-//     speed: null,
-//     model: '',
-//     key: 'qwertyu',
-//     someProp: {
-//         prop1: 'qwerty',
-//         prop2: undefined
-//     }
-// };
-
-// console.log(filterWithClone(car));
-
-// filter(car);
-// console.log(car);
-// let key = 'color';
-// car[key]; // car['color']
-// car['color']; // car.color;
-
-// function someFunction(...rest) {
-//     someFunction2(...rest);
-// }
-
-// function someFunction2(a, b, c) {
-//     console.log('someFunction2: ', a);
-//     console.log('someFunction2: ', b);
-//     console.log('someFunction2: ', c);
-// }
-
-// someFunction(1, 2, 3, 4, 5, 6);
-
-function clonePart(obj, ...keys) {
-    const clone = {};
-
-    for (let key in obj) {
-        if (keys.includes(key)) {
-            clone[key] = obj[key];
-        }
+const object2 = {
+    a: 1,
+    b: 2,
+    toString: function() {
+        return `${this.a} + ${this.b} = ${this.a + this.b}`;
     }
+};
 
-    return clone;
+// console.log(object2.toString());
+
+const object3 = {
+    a: 5,
+    b: 6,
+    toString: object2.toString
 }
 
-let comment = {
-    type: 'success',
-    message: 'text-text',
-    authorId: 'qwerty'
+// console.log(o);
+// console.log(object2);
+// console.log(object3)
+
+// console.log(object2.toString());
+// console.log(object3.toString());
+
+object3.newProperty = "NewProperty";
+object3.newObj = {a: 123}
+
+// console.log(object3);
+
+delete object3.newObj;
+delete object3.toString;
+delete object3.newProperty;
+
+// console.log(object3);
+
+// Расширение объектов 
+
+const obj1 = {
+    a: 1,
+    b: 2,
+}
+
+const obj2 = {
+    c: 3,
+    d: 4
 };
-let cl = clonePart(comment, 'message', 'authorId'); 
 
-/**
- * написать функцию filter которая принимает в качестве аргумента любой объект
- * и удаляет в нем свойства, значение которых = undefined
- * 
- * Например: filter({ color: 'black', model: 'mercedes', speed: undefined }); // { color: 'black', model: 'mercedes' }
- * как сделать так, чтобы функция возвращала новый объект?
- * как сделать глубокую фильтрацию свойств объекта?
- * 
- * написать функцию clone, которая принимает в качестве аргумента объект и возвращает новый объект с такой же структурой
- * Например:
- * const nick = { name: 'Nick', age: 20 };
- * const notNick = clone(nick);
- * console.log(nick === notNick); // false
- * 
- * написать функцию clonePart которая принимает первым аргументом объект, а последующими - строки (колличество аргументов не ограничено)
- * clonePart должна вернуть новый объект со свойствами которые совпадают со значениями аргументов функции
- * Например:
- * clonePart({ type: 'success', message: 'text-text', authorId: 'qwerty' }, 'message', 'authorId'); // вернет новый объект => { message: 'text-text', authorId: 'qwerty' }
- */
+const obj3 = {
+    ...obj1,
+    ...obj2,
+    abc: "123"
+};
 
-// методы объекта, cокращённая запись метода *
+const obj4 = Object.assign({}, obj1, obj2, obj3);
 
-// let age = 44;
+// console.log(Object.entries(obj2));
 
-// let user = {
-//     name: 'Bob',
-//     age,
-//     sayHello: function() {
-//         console.log('Hello!');
-//     },
-//     sayHi() {
-//         console.log('Hi ' + this.name);
-//     }
-// };
+// console.log(JSON.stringify(obj1));
 
-// let foo = user.sayHi;
+const strObj = JSON.stringify(obj1);
 
-// foo();
-
-// Ключевое слово «this» в методах *
-
-// потеря this - ссылочный тип
+// console.log(JSON.parse(strObj));
 
 
+// Конструкторы this
 
-// преобразование объектов в примитивы string, number, default *
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+    this.output = () => console.log(name, age)
+};
 
-// let someObj = {
-//     [Symbol.toPrimitive](type) {
-//         switch(type) {
-//             case 'string':
-//                 return 'this is string';
-//             case 'number':
-//                 return 123456;
-//             case 'default':
-//                 return 'this, is default';
-//         }
-//     }
-// };
+const person = new Person('Ivan', 23);
+const person2 = new Person('Sasha', 28);
+person.output()
+person2.output()
 
-// let simeObj2 = {
-//     age: 5000,
+const newObject = {a:1, b:2}
 
-//     toString() {
-//         return this.age;
-//     },
-//     valueOf() {
-        
-//     }
-// };
+console.log(person);
+console.log(person2);
+console.log(newObject);
 
-// console.log(String(simeObj2));
-// console.log(Number(simeObj2));
-// console.log('!!!'+ simeObj2);
-// console.log(simeObj2.toString());
+const withoutPrototypes = Object.create(null);
 
-// Конструкторы, создание объектов через "new" *
-
-// let o = {
-
-// };
-
-// function Car() {
-//     // let obj = {}; => this
-
-//     // if (!new.target) {
-//     //     return new Car();
-//     // }
-
-//     this.model = 'honda';
-//     this.speed = 180;
-//     this.state = 'stopped';
-
-//     this.run = function () {
-//         this.state = 'running';
-//     }
-
-//     this.stop = function() {
-//         this.state = 'stopped';
-//     }
-
-//     // return obj; this
-// }
-
-// let car = new Car();
-// let car2 = new Car();
-// let car3 = new Car();
-// let car4 = new Car();
-
-// console.log('car: ', car);
-
-// let car = {
-//     state: 'stopped',
-//     model: 'ГАЗ'
-// };
-
-// car.ran = function() {
-//     this.state = 'running';
-// }
-
-// car.stop = function() {
-//     this.state = 'stopped';
-// }
-
-// console.log(car);
-// car.ran();
-// console.log(car);
-// car.stop();
-// console.log(car);
-
-
-// let item = {
-//     label: 'kettle',
-//     power: 1300,
-//     [Symbol.toPrimitive](type) {
-//         return type === 'string'
-//          ? `${this.label} - ${this.power} watt`
-//          : this.power;
-//     }
-// };
-
-// console.log(String(item));
-// console.log(+item);
-// console.log('' + item);
-
-// function Cat(name) {
-//     this.name = name;
-//     this.state = 'lying';
-//     this.color = 'red';
-
-//     this.meow = function() {
-//         console.log(this.name + ': meow');
-//     };
-
-//     this.up = function() {
-//         this.state = 'standing';
-//     }
-
-//     this.down = function() {
-//         this.state = 'lying';
-//     };
-// }
-
-// let cat = new Cat('tuzik');
-
-// console.log(cat);
-// cat.meow();
-// cat.up();
-// console.log(cat);
-// cat.down();
-// console.log(cat);
-
-/**
- * есть объект car = { state: 'stopped', model: 'ГАЗ' }
- * добавьте метод run(); при вызове которого state должно меняться на running
- * добавьте метод stop(); при вызове которого state должно меняться на stopped
- * 
- * есть объект item = { label: 'kettle', power: 1300 }
- * cделать так чтобы при преобразовании объекта к числу возвращалось поле power
- * а при преобразовании к строке - строка вида "kettle - 1300 watt"
- * 
- * написать конструктор Cat которой создает объекты со свойствами name, state, color
- * и методами
- * 
- * cat.meow(); // => выводит в консоль "{{имя_кошки}}: meow"
- * cat.down(); // меняет свойство state на строку 'lying'
- * cat.up(); // меняет свойство state на строку 'standing'
- */
+console.log(withoutPrototypes);
